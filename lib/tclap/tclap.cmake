667948e47cf397280bcc12bba3bab9a9072fbd78
Wohlstand <admin@wohlnet.ru>
2020-02-18 02:31:23 +0300

    Added tclap library

---
+++ lib/tclap/tclap.cmake
@@ -0,0 +1,3 @@
+# message("Path to TClap is [${CMAKE_CURRENT_LIST_DIR}], has no sources: only includes")
+include_directories(${CMAKE_CURRENT_LIST_DIR}/)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/..)
