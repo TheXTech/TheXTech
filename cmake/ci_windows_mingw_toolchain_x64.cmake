ca25efbd5dc72c598b1f2e01f9e43833ea551581
Wohlstand <admin@wohlnet.ru>
2020-12-15 12:04:43 +0300

    CI: Try to pass compiler options via toolchain file

---
+++ cmake/ci_windows_mingw_toolchain_x64.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_MAKE_PROGRAM "C:/WohlMinGWw64/mingw64/bin/ninja.exe")
+set(CMAKE_C_COMPILER "C:/WohlMinGWw64/mingw64/bin/gcc.exe")
+set(CMAKE_CXX_COMPILER "C:/WohlMinGWw64/mingw64/bin/g++.exe")
