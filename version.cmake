99aad82757cdfa79b26a12d7025c10aba22d25f5
Wohlstand <admin@wohlnet.ru>
2021-01-09 01:11:26 +0300

    Set version into 1.3.3.2

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set(THEXTECH_VERSION_2 3)
 # Revision
 set(THEXTECH_VERSION_3 3)
 # Patch
-set(THEXTECH_VERSION_4 1)
+set(THEXTECH_VERSION_4 2)
 # Type of version: "-alpha","-beta","-dev", or "" aka "release"
 set(THEXTECH_VERSION_REL "")
 
