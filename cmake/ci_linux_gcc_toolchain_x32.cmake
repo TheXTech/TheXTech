131dc1e8352206b7bca7499a41a7264f57295474
Wohlstand <admin@wohlnet.ru>
2021-04-26 22:50:10 +0300

    Attempt to fix 32-bit Linux build number 5

--- cmake/ci_linux_gcc_toolchain_x32.cmake
+++ cmake/ci_linux_gcc_toolchain_x32.cmake
@@ -1,9 +1,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_C_FLAGS -m32)
-set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_CXX_FLAGS -m32)
+set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER i686-linux-gnu-g++)
+set(CMAKE_AR i686-linux-gnu-ar)
 
 set(CMAKE_FIND_ROOT_PATH /usr/lib/i386-linux-gnu/)
 
