1f544d413e83a944092f30a36364b60eb5ad87e7
Wohlstand <admin@wohlnet.ru>
2021-07-06 13:23:24 +0300

    Don't use tinyfiledialogs on Emscripten
    TODO: Replace the whole thing with the game friendly input dialog, the same as at Moondust Engine

--- src/editor/text_input.cpp
+++ src/editor/text_input.cpp
@@ -1,15 +1,28 @@
-#include <tinyfiledialogs/tinyfiledialogs.h>
 #include "../globals.h"
 #include "../sound.h"
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <Logger/logger.h>
+#else
+#include <tinyfiledialogs/tinyfiledialogs.h>
+#endif
 
 const std::string GetTextInput(const std::string& prompt, const std::string& init = "")
 {
     SoundPauseAll();
     const char* get;
+
+#ifndef __EMSCRIPTEN__
     if (init.empty())
         get = tinyfd_inputBox(prompt.c_str(), nullptr, " ");
     else
         get = tinyfd_inputBox(prompt.c_str(), nullptr, init.c_str());
+#else
+    // FIXME: Implement the Emscripten-side support
+    get = nullptr;
+    pLogWarning("Calling GetTextInput() with the missing Emscripten implementation");
+#endif
+
     SoundResumeAll();
     if (get == nullptr)
         return "";
