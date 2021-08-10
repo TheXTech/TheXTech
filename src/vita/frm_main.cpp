822d847955e92758527b5e81ac018e342cf426ed
Mike Santiago <miketheripper1@gmail.com>
2021-08-09 21:13:10 -0400

    [VITA] GL_CLAMP_TO_EDGE for textures

--- src/vita/frm_main.cpp
+++ src/vita/frm_main.cpp
@@ -646,6 +646,9 @@ void FrmMain::loadTexture(StdPicture &target, uint32_t width, uint32_t height, u
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     // TODO: Do the responsible thing and update this
     // To support taking the format from the texture.
     // Also using its number of colors properly.
