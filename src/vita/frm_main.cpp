632bad4ee545e5c4debc07f0596b0855b3fcb9c6
Mike Santiago <axiom@ignoresolutions.xyz>
2021-08-28 20:20:06 -0400

    [VITA] Remove erroneously added function during merge

--- src/vita/frm_main.cpp
+++ src/vita/frm_main.cpp
@@ -1292,11 +1292,6 @@ void FrmMain::eventMouseMove(SDL_MouseMotionEvent &event)
     }
 }
 
-void FrmMain::makeShot()
-{
-    return;
-}
-
 void FrmMain::eventMouseUp(SDL_MouseButtonEvent &event)
 {
     bool doubleClick = false;
