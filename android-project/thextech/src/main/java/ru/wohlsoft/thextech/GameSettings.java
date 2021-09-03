a622f280c785a4ca3aa4df32a2d3f335521118b7
Wohlstand <admin@wohlnet.ru>
2021-09-03 21:25:35 +0300

    Android: Test the feedback vibration at menu
    Added an ability to test the vibrator feedback from the setting menu directly

--- android-project/thextech/src/main/java/ru/wohlsoft/thextech/GameSettings.java
+++ android-project/thextech/src/main/java/ru/wohlsoft/thextech/GameSettings.java
@@ -1,12 +1,12 @@
 package ru.wohlsoft.thextech;
 
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 
 import java.io.File;
 
@@ -55,6 +55,43 @@ public class GameSettings extends AppCompatActivity
                         if(ctx == null)
                             return false;// Null context, not allowed!
                         return selectAssetsPath(ctx, null);
+                    }
+                });
+            }
+
+            Preference fb_test = getPreferenceManager().findPreference("touchscreen_vibration_test");
+            if (fb_test != null)
+            {
+                fb_test.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+                {
+                    @Override
+                    public boolean onPreferenceClick(Preference arg0)
+                    {
+                        Context ctx = getContext();
+                        if(ctx == null)
+                            return false;// Null context, not allowed!
+
+                        SharedPreferences setup = PreferenceManager.getDefaultSharedPreferences(ctx);
+                        float vibratorStrength = Float.parseFloat(setup.getString("touchscreen_vibration_strength", "1.0"));
+                        int vibratorLength = Integer.parseInt(setup.getString("touchscreen_vibration_length", "12"));
+
+                        int vibeValue = Math.round(vibratorStrength * 255);
+
+                        Vibrator v = (Vibrator)ctx.getSystemService(Context.VIBRATOR_SERVICE);
+
+                        if (vibeValue > 255) {
+                            vibeValue = 255;
+                        }
+                        if (vibeValue < 1) {
+                            return false;
+                        }
+                        // Vibrate for X milliseconds
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                            v.vibrate(VibrationEffect.createOneShot(vibratorLength, vibeValue));
+                        else
+                            v.vibrate(vibratorLength); //deprecated in API 26
+
+                        return true;
                     }
                 });
             }
