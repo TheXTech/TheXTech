ffe4d864b0e60f0905a2140f25ff13a3738457c2
Wohlstand <admin@wohlnet.ru>
2021-09-10 20:44:08 +0300

    Fixed the small bug of Yoshi Mode
    Yoshi mode track won't get muted when jump off Yoshi underwater

--- src/player/player_update.cpp
+++ src/player/player_update.cpp
@@ -1223,6 +1223,7 @@ void UpdatePlayer()
                             Player[A].CanJump = false;
                             Player[A].StandingOnNPC = 0;
                             Player[A].Mount = 0;
+                            UpdateYoshiMusic();
                             numNPCs++;
                             NPC[numNPCs] = NPC_t();
                             NPC[numNPCs].Direction = Player[A].Direction;
@@ -1937,6 +1938,7 @@ void UpdatePlayer()
                     {
                         PlayerHurt(A);
                         Player[A].Mount = 0;
+                        UpdateYoshiMusic();
                     }
                     if(Player[A].Slide)
                         Player[A].Slide = false;
