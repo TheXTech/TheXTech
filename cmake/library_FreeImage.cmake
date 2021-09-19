6091390969ff4bb46811b37ba004838ec03f3ba7
Wohlstand <admin@wohlnet.ru>
2021-09-19 04:27:48 +0300

    CMake: Added the flag for PNG HW optimizations

--- cmake/library_FreeImage.cmake
+++ cmake/library_FreeImage.cmake
@@ -4,6 +4,7 @@ add_library(PGE_FreeImage INTERFACE)
 
 option(USE_SHARED_FREEIMAGE "Use shared build of FreeImage" OFF)
 option(USE_FREEIMAGE_SYSTEM_LIBS "Let FreeImage to use libPNG and libJPEG from the system" OFF)
+option(USE_PNG_HARDWARE_OPTIMIZATIONS "Enable hardware optimizations for the FreeImage internal build of libPNG" ON)
 
 if(USE_SHARED_FREEIMAGE)
     set_shared_lib(libFreeImage_Libs "${DEPENDENCIES_INSTALL_DIR}/lib" FreeImageLite)
@@ -34,6 +35,7 @@ ExternalProject_Add(
         "-DCMAKE_DEBUG_POSTFIX=${PGE_LIBS_DEBUG_SUFFIX}"
         "-DFREEIMAGE_USE_SYSTEM_LIBPNG=${USE_FREEIMAGE_SYSTEM_LIBS}"
         "-DFREEIMAGE_USE_SYSTEM_LIBJPEG=${USE_FREEIMAGE_SYSTEM_LIBS}"
+        "-DPNG_HARDWARE_OPTIMIZATIONS=${USE_PNG_HARDWARE_OPTIMIZATIONS}"
         #"-DFREEIMAGE_PNG_INCLUDE=${PNG_INCLUDE_DIRS}"
         #"-DFREEIMAGE_ZLIB_INCLUDE=${ZLIB_INCLUDE_DIRS}"
         #"-DFREEIMAGE_PNG_LIB=${libPNG_A_Lib}"
