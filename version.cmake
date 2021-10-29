33731bcb39e68aa14eeaaec16cec405c905c987a
Wohlstand <admin@wohlnet.ru>
2021-10-29 04:36:30 +0300

    Bump version to 1.3.5.3
    Until the devel backporting work will be actually started, keep a minor update yet

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set(THEXTECH_VERSION_2 3)
 # Revision
 set(THEXTECH_VERSION_3 5)
 # Patch
-set(THEXTECH_VERSION_4 2)
+set(THEXTECH_VERSION_4 3)
 # Type of version: "-alpha","-beta","-dev", or "" aka "release"
 set(THEXTECH_VERSION_REL "")
 
