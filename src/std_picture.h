d1d00430d521155df6b2c0fbb9763933fd518b22
Wohlstand <admin@wohlnet.ru>
2022-01-02 10:26:08 +0300

    std_pucture.h: Add documenting comments

--- src/std_picture.h
+++ src/std_picture.h
@@ -24,13 +24,16 @@
 
 #include <vector>
 #ifdef DEBUG_BUILD
-#include <string>
+#   include <string>
 #endif
 
 typedef unsigned int    GLenum;
 typedef int             GLint;
 typedef unsigned int    GLuint;
 
+/*!
+ * \brief RGB pixel color
+ */
 struct PGEColor
 {
     float r = 0.0f;
@@ -39,34 +42,65 @@ struct PGEColor
 };
 
 struct SDL_Texture;
+
+/**
+ * @brief Handler of a graphical texture
+ */
 struct StdPicture
 {
 #ifdef DEBUG_BUILD
+    //! Debug-only file path to the original picture
     std::string origPath;
 #endif
+    //! Was this texture initialized by graphical engine or not?
     bool inited = false;
-    // Width and height
+
+    //! Width of texture
     int w = 0;
+    //! Height of texture
     int h = 0;
+
     // Frame width and height (for animation sprite textures)
+    //! Animation frame width
     int frame_w = 0;
+    //1 Animation frame height
     int frame_h = 0;
+
     // Original size (if texture got scaled while loading)
+    //! Original width
     int w_orig = 0;
+    //! Original height
     int h_orig = 0;
+
     // Difference between original and initial size
+    //! Width scale factor
     float w_scale = 1.0f;
+    //! Height scale factor
     float h_scale = 1.0f;
 
+    //! Is this a lazy-loaded texture?
     bool lazyLoaded = false;
+    //! Original compressed data of the front image
     std::vector<char> raw;
+    //! Original compressed data of the mask image (if presented)
     std::vector<char> rawMask;
+
+    //! Was mask restored from the PNG at default graphics?
     bool isMaskPng = false;
+    //! Texture instance pointer for SDL Render
     SDL_Texture *texture = nullptr;
+    //! texture ID for OpenGL and other render engines
+    GLint        texture_id = 0;
+
+    //! Texture format at OpenGL-renderer
     GLenum format = 0;
+    //! Number of colors
     GLint  nOfColors = 0;
+    //! Left-top pixel color
     PGEColor ColorUpper;
+    //! Left-bottom pixel color
     PGEColor ColorLower;
+    //! Cached color modifier
     uint8_t modColor[4] = {255,255,255,255};
 };
 
