264709a1d8f79e99a23c16e6cadef93b3f71cbe0
Wohlstand <admin@wohlnet.ru>
2022-01-13 13:10:53 +0300

    Luna: Added license header and describe level codes

--- src/script/lunacell.cpp
+++ src/script/lunacell.cpp
@@ -1,3 +1,23 @@
+/*
+ * TheXTech - A platform game engine ported from old source code for VB6
+ *
+ * Copyright (c) 2009-2011 Andrew Spinks, original VB6 code
+ * Copyright (c) 2020-2021 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cmath>
 #include <vector>
 
@@ -332,6 +352,7 @@ void CellManager::SortByNearest(std::list<CellObj> *objlist, double cx, double c
     double lowest_dist = 9999999;
     int lowest_index = 0;
     unsigned int extent = 0;
+
     while(extent < objvec.size())
     {
         for(unsigned int i = extent; i < objvec.size(); i++)
--- src/script/lunacounter.h
+++ src/script/lunacounter.h
@@ -1,3 +1,23 @@
+/*
+ * TheXTech - A platform game engine ported from old source code for VB6
+ *
+ * Copyright (c) 2009-2011 Andrew Spinks, original VB6 code
+ * Copyright (c) 2020-2021 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 #ifndef DEATHCOUNTER_H
 #define DEATHCOUNTER_H
