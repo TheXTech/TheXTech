a173733d2386644f1d18524ceb5ad3570f8ee22e
Wohlstand <admin@wohlnet.ru>
2022-01-13 04:40:46 +0300

    Small tune of renderOp classes

--- src/script/renderop_rect.cpp
+++ src/script/renderop_rect.cpp
@@ -5,16 +5,20 @@
 
 void RenderRectOp::Draw(Renderer *renderer)
 {
-    if (borderColor.a <= 0.0 && fillColor.a <= 0.0) return;
+    if(borderColor.a <= 0.0 && fillColor.a <= 0.0) return;
 
     // Convert coordinates
     double x1 = this->x1, y1 = this->y1, x2 = this->x2, y2 = this->y2;
-    if (sceneCoords) {
+    if(sceneCoords)
+    {
         x1 -= vScreenX[renderer->GetCameraIdx()];
         y1 -= vScreenY[renderer->GetCameraIdx()];
         x2 -= vScreenX[renderer->GetCameraIdx()];
         y2 -= vScreenY[renderer->GetCameraIdx()];
     }
+
+    if(!sceneCoords)
+        XRender::offsetViewportIgnore(true);
 
     if(fillColor.a > 0.0f)
         XRender::renderRect(x1, y1, x2 - x1, y2 - y1,
@@ -23,4 +27,8 @@ void RenderRectOp::Draw(Renderer *renderer)
     if(borderColor.a > 0.0f)
         XRender::renderRect(x1, y1, x2 - x1, y2 - y1,
                             borderColor.r, borderColor.g, borderColor.b, borderColor.a, false);
+
+    if(!sceneCoords)
+        XRender::offsetViewportIgnore(false);
 }
+
--- src/script/renderop_string.h
+++ src/script/renderop_string.h
@@ -9,29 +9,13 @@ class RenderStringOp : public RenderOp
 {
 public:
     // Quick ctor
-    RenderStringOp() : RenderStringOp("", 1, 400.f, 400.f) {}
+    RenderStringOp();
 
-    RenderStringOp(const std::string &str, short font_type, float X, float Y) :
-        RenderOp(RENDEROP_DEFAULT_PRIORITY_TEXT),
-        m_String(str),
-        m_FontType(font_type),
-        m_X(X),
-        m_Y(Y),
-        sceneCoords(false)
-    {}
+    RenderStringOp(const std::string &str, short font_type, float X, float Y);
 
-    virtual ~RenderStringOp() { }
-    virtual void Draw(Renderer* renderer)
-    {
-//        VB6StrPtr text(m_String);
-        float x = m_X, y = m_Y;
-        if(sceneCoords)
-        {
-            x -= vScreenX[renderer->GetCameraIdx()];
-            y -= vScreenY[renderer->GetCameraIdx()];
-        }
-        SuperPrint(m_String, m_FontType, x, y);
-    }
+    virtual ~RenderStringOp() = default;
+
+    virtual void Draw(Renderer *renderer);
 
     std::string m_String;
     short m_FontType;
