2eaea94c7b1f459051f4f0a221d10f8e3475fb13
Wohlstand <admin@wohlnet.ru>
2022-01-16 04:16:43 +0300

    Update lunarender.h
    Beautify

--- src/script/luna/lunarender.h
+++ src/script/luna/lunarender.h
@@ -32,7 +32,7 @@ class LunaImage;
 
 struct Renderer
 {
-    static Renderer& Get();
+    static Renderer &Get();
     static void SetAltThread();
     static void UnsetAltThread();
     static bool IsAltThreadActive();
@@ -40,19 +40,19 @@ struct Renderer
     Renderer() noexcept;
     ~Renderer() = default;
 
-    bool LoadBitmapResource(const std::string& filename, int resource_code, int transparency_color); // don't give full path
-    bool LoadBitmapResource(const std::string& filename, int resource_code);
-    void StoreImage(const std::shared_ptr<LunaImage>& bmp, int resource_code);
+    bool LoadBitmapResource(const std::string &filename, int resource_code, int transparency_color); // don't give full path
+    bool LoadBitmapResource(const std::string &filename, int resource_code);
+    void StoreImage(const std::shared_ptr<LunaImage> &bmp, int resource_code);
     bool DeleteImage(int resource_code);
     std::shared_ptr<LunaImage> GetImageForResourceCode(int resource_code);
 
-    std::vector<std::shared_ptr<LunaImage>> LoadAnimatedBitmapResource(const std::string& filename, int* frameTime = 0);
+    std::vector<std::shared_ptr<LunaImage>> LoadAnimatedBitmapResource(const std::string &filename, int *frameTime = 0);
 
-    void AddOp(RenderOp* op);							// Add a drawing operation to the list
+    void AddOp(RenderOp *op);                           // Add a drawing operation to the list
     // void GLCmd(const std::shared_ptr<GLEngineCmd>& cmd, double renderPriority = 1.0);
 
-    void DebugPrint(const std::string &message);				// Print a debug message on the screen
-    void DebugPrint(const std::string &message, double val);	// Print a debug message on the screen and display a related value
+    void DebugPrint(const std::string &message);                // Print a debug message on the screen
+    void DebugPrint(const std::string &message, double val);    // Print a debug message on the screen and display a related value
 
     void RenderBelowPriority(double maxPriority);
 
@@ -66,7 +66,7 @@ struct Renderer
 
     void ClearQueue();
 private:
-    void DrawOp(RenderOp& render_operation);
+    void DrawOp(RenderOp &render_operation);
 
 
     // Members //
@@ -79,7 +79,7 @@ public:
 
         std::size_t m_renderOpsSortedCount;
         std::size_t m_renderOpsProcessedCount;
-        std::vector<RenderOp*> m_currentRenderOps;  // render operations to be performed
+        std::vector<RenderOp *> m_currentRenderOps; // render operations to be performed
 
         std::list<std::string> m_debugMessages;    // Debug message to be printed
 
@@ -101,12 +101,16 @@ private:
 
     // Simple getters //
 public:
-    int GetCameraIdx() const { return m_queueState.m_curCamIdx; }
+    int GetCameraIdx() const
+    {
+        return m_queueState.m_curCamIdx;
+    }
     // HDC GetScreenDC() { return (HDC)GM_SCRN_HDC; }
 
-    class QueueStateStacker {
+    class QueueStateStacker
+    {
     private:
-        Renderer& m_renderer;
+        Renderer &m_renderer;
         QueueState m_savedState;
 
     public:
@@ -135,7 +139,7 @@ namespace Render
 {
 
 bool IsOnScreen(double x, double y, double w, double h); // Returns whether or not the digven rectangle is on screen this frame
-void CalcCameraPos(double* p_X, double* p_Y);            // Tries to read smbx memory to return the camera coords in the 2 passed args
+void CalcCameraPos(double *p_X, double *p_Y);            // Tries to read smbx memory to return the camera coords in the 2 passed args
 
 }
 
