2fe9bbe6122c4c6ce32333192d24a6e3b33b4bb4
Wohlstand <admin@wohlnet.ru>
2022-06-14 15:22:53 +0300

    Rename "app_path.cpp" into "app_path_old.cpp"
    Prepare to the gradual rewriting of the module:
    - Use old module on platforms where it wasn't updated yet
    - Use new module once it has been created for a new platform

--- lib/AppPath/app_path.cmake
+++ lib/AppPath/app_path.cmake
@@ -1,11 +1,13 @@
 set(APPPATH_SRCS)
 
 list(APPEND APPPATH_SRCS
-    ${CMAKE_CURRENT_LIST_DIR}/private/app_path.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/app_path.h
+    ${CMAKE_CURRENT_LIST_DIR}/private/app_path_old.cpp
 )
 
 if(APPLE)
     list(APPEND APPPATH_SRCS
+        ${CMAKE_CURRENT_LIST_DIR}/private/app_path_macosx.h
         ${CMAKE_CURRENT_LIST_DIR}/private/app_path_macosx.m
     )
 endif()
