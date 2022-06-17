fd77a46bef9572d3d8cdd71e8fb447dcda5011fd
Wohlstand <admin@wohlnet.ru>
2022-06-17 18:16:59 +0300

    record.cpp: Fixed the Windows support There is a lot of warnings caused by wrong PRI64 definition. So, now it has been fixed.

--- src/main/record.cpp
+++ src/main/record.cpp
@@ -56,15 +56,25 @@
 
 #ifndef PRId64 /*Workaround*/
 #   ifndef __PRI64_PREFIX
-#       if __WORDSIZE == 64
-#           define __PRI64_PREFIX	"l"
-#           define __PRIPTR_PREFIX	"l"
+#       if THEXTECH_WORDSIZE == 64
+#           ifdef _WIN32
+#               define __PRI64_PREFIX   "I64"
+#               define __PRIPTR_PREFIX  "I64"
+#               ifdef __MINGW32__
+#                   pragma GCC diagnostic push
+#                   pragma GCC diagnostic ignored "-Wformat="
+#                   define X_GCC_NO_WARNING_FORMAT
+#               endif
+#           else
+#               define __PRI64_PREFIX   "l"
+#               define __PRIPTR_PREFIX  "l"
+#           endif
 #       else
-#           define __PRI64_PREFIX	"ll"
+#           define __PRI64_PREFIX   "ll"
 #           define __PRIPTR_PREFIX
 #       endif
 #   endif
-#   define PRId64		__PRI64_PREFIX "d"
+#   define PRId64   __PRI64_PREFIX "d"
 #endif
 
 static std::string makeRecordPrefix()
@@ -887,3 +897,7 @@ void Sync()
 }
 
 } // namespace Record
+
+#ifdef X_GCC_NO_WARNING_FORMAT
+#   pragma GCC diagnostic pop
+#endif
