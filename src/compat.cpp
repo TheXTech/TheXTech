db4b49325e6eeaef8b81158710bebaa23659398b
Wohlstand <admin@wohlnet.ru>
2022-07-16 01:28:35 +0300

    compat.cpp: Fixed build and added missing reader

--- src/compat.cpp
+++ src/compat.cpp
@@ -93,7 +93,7 @@ static void compatInit(Compatibility_t &c)
     c.luna_enable_engine = Compatibility_t::LUNA_ENGINE_UNSPECIFIED;
     c.fix_fairy_stuck_in_pipe = true;
     c.world_map_fast_move = false;
-    c.fix_flamethrower_gravity = true
+    c.fix_flamethrower_gravity = true;
 
 
     if(s_compatLevel >= COMPAT_SMBX2) // Make sure that bugs were same as on SMBX2 Beta 4 on this moment
@@ -276,6 +276,7 @@ static void loadCompatIni(Compatibility_t &c, const std::string &fileName)
     compat.read("fix-npc-activation-event-loop-bug", c.fix_npc_activation_event_loop_bug, c.fix_npc_activation_event_loop_bug);
     // 1.3.6
     compat.read("world-map-fast-move", c.world_map_fast_move, c.world_map_fast_move);
+    compat.read("fix-framethrower-gravity", c.fix_flamethrower_gravity, c.fix_flamethrower_gravity);
     compat.endGroup();
 }
 
