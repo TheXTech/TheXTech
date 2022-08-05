ef16985fbfddc72c8491ddb9c5570a3e90f634f0
Wohlstand <admin@wohlnet.ru>
2022-08-05 22:49:08 +0300

    npc_update.cpp: Use NPCID_SKULL for raft cells

--- src/npc/npc_update.cpp
+++ src/npc/npc_update.cpp
@@ -1872,7 +1872,7 @@ void UpdateNPCs()
                         NPC[A].Location.SpeedY = 0;
                     }
 
-                    if(NPC[A].Type == 190) // Skull raft
+                    if(NPC[A].Type == NPCID_SKULL) // Skull raft
                     {
                         for(B = 1; B <= numPlayers; B++)
                         {
@@ -2174,7 +2174,7 @@ void UpdateNPCs()
                                                         if(NPC[A].Type == 48 && (Block[B].IsNPC == 22 || Block[B].IsNPC == 49)) // spiney eggs don't walk on special items
                                                             HitSpot = 0;
 
-                                                        if(NPC[A].Type == 190) // Skull raft
+                                                        if(NPC[A].Type == NPCID_SKULL) // Skull raft
                                                         {
                                                             if(Block[B].IsNPC > 0)
                                                                 HitSpot = 0;
