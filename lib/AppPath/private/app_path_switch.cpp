231a32cfd5f23d8afbee4a66b09a10b0c2fcf6c5
Wohlstand <admin@wohlnet.ru>
2022-09-16 05:06:01 +0300

    Initial attempt to run on Nintendo Switch
    Doesn't runs yet

---
+++ lib/AppPath/private/app_path_switch.cpp
@@ -0,0 +1,87 @@
+/*
+ * Moondust, a free game engine for platform game making
+ * Copyright (c) 2014-2022 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This software is licensed under a dual license system (MIT or GPL version 3 or later).
+ * This means you are free to choose with which of both licenses (MIT or GPL version 3 or later)
+ * you want to use this software.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You can see text of MIT license in the LICENSE.mit file you can see in Engine folder,
+ * or see https://mit-license.org/.
+ *
+ * You can see text of GPLv3 license in the LICENSE.gpl3 file you can see in Engine folder,
+ * or see <http://www.gnu.org/licenses/>.
+ */
+
+#include "app_path_private.h"
+
+static std::string s_assetsRoot;
+static std::string s_userDirectory;
+static std::string s_applicationPath;
+
+
+void AppPathP::initDefaultPaths(const std::string & /*userDirName*/)
+{
+    s_applicationPath = "/TheXTech/";
+    s_assetsRoot = s_applicationPath;
+    s_userDirectory = s_applicationPath;
+}
+
+std::string AppPathP::appDirectory()
+{
+    return s_applicationPath;
+}
+
+std::string AppPathP::userDirectory()
+{
+    return s_userDirectory;
+}
+
+std::string AppPathP::assetsRoot()
+{
+    return s_assetsRoot;
+}
+
+std::string AppPathP::screenshotsRoot()
+{
+    /*
+     * Fill this in only condition when you want to use the system-wide screenshots
+     * directory out of user directory. Keep it empty if you want to keep the
+     * default behaviour (i.e. screenshots saved at the user directory)
+     */
+    return std::string();
+}
+
+std::string AppPathP::gifRecsRoot()
+{
+    /*
+     * Fill this in only condition when you want to use the system-wide GIF recordings
+     * directory out of user directory. Keep it empty if you want to keep the
+     * default behaviour (i.e. GIF recordings saved at the user directory)
+     */
+    return std::string();
+}
+
+std::string AppPathP::logsRoot()
+{
+    /*
+     * Fill this in only condition when you want to use the system-wide logs
+     * directory out of user directory. Keep it empty if you want to keep the
+     * default behaviour (i.e. logs saved at the user directory)
+     */
+    return std::string();
+}
+
+bool AppPathP::portableAvailable()
+{
+    return false;
+}
+
+void AppPathP::syncFS()
+{
+    /* Run the FS synchronization (Implement this for Emscripten only) */
+}
