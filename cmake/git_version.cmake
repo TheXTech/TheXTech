ff86fa0557c66ef1901d86993627d25574cd2347
Wohlstand <admin@wohlnet.ru>
2022-11-15 03:51:23 +0300

    CMake: Set GIT revision tag to few files only
    This should avoid the full rebuild of the project on every git commit/pull (that causes GIT revision being altered). This is very painful on old/weak platforms such as ARM boards and Power PC based Mac Mini.

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -1,13 +1,24 @@
 
 if(NOT GIT_COMMIT_HASH)
-    add_definitions(-DGIT_VERSION="CMakeUnknown")
+    set(THEXTECH_GIT_VERSION "CMakeUnknown")
 else()
-    add_definitions(-DGIT_VERSION="${GIT_COMMIT_HASH}")
+    set(THEXTECH_GIT_VERSION "${GIT_COMMIT_HASH}")
 endif()
 
 if(NOT GIT_BRANCH)
-    add_definitions(-DGIT_BRANCH="unknown")
+    set(THEXTECH_GIT_BRANCH "unknown")
 else()
-    add_definitions(-DGIT_BRANCH="${GIT_BRANCH}")
+    set(THEXTECH_GIT_BRANCH "${GIT_BRANCH}")
 endif()
 
+macro(add_git_version_defs FILETARGET)
+    set_property(
+        SOURCE ${FILETARGET}
+        APPEND PROPERTY COMPILE_DEFINITIONS GIT_VERSION="${THEXTECH_GIT_VERSION}"
+    )
+    set_property(
+        SOURCE ${FILETARGET}
+        APPEND PROPERTY COMPILE_DEFINITIONS GIT_BRANCH="${THEXTECH_GIT_BRANCH}"
+    )
+    message("-- Set GIT definitions to file ${FILETARGET}")
+endmacro()
