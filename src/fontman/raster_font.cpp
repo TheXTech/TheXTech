a244b547ad327f9223137d42ad79852caedfc3dd
Wohlstand <admin@wohlnet.ru>
2023-01-04 21:11:30 +0300

    Moore files got an updated copyright for 2023

--- src/fontman/raster_font.cpp
+++ src/fontman/raster_font.cpp
@@ -1,6 +1,6 @@
 /*
  * Moondust, a free game engine for platform game making
- * Copyright (c) 2014-2021 Vitaly Novichkov <admin@wohlnet.ru>
+ * Copyright (c) 2014-2023 Vitaly Novichkov <admin@wohlnet.ru>
  *
  * This software is licensed under a dual license system (MIT or GPL version 3 or later).
  * This means you are free to choose with which of both licenses (MIT or GPL version 3 or later)
