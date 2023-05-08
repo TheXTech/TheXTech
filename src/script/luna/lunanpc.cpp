ce7de6e0b2c9046a53fef4f6184c03b90c58a211
Wohlstand <admin@wohlnet.ru>
2023-05-08 14:52:51 +0300

    lunanpc.cpp: Update tree if position/size changed

--- src/script/luna/lunanpc.cpp
+++ src/script/luna/lunanpc.cpp
@@ -22,6 +22,7 @@
 #include "globals.h"
 #include "mememu.h"
 #include "npc.h"
+#include "main/trees.h"
 
 
 NPC_t *NpcF::Get(int index)
@@ -86,7 +87,16 @@ void NpcF::MemSet(int ID, size_t offset, double value, OPTYPE operation, FIELDTY
     {
         thisnpc = &NPC[i];
         if(anyID || thisnpc->Type == ID)
+        {
+            auto ol = thisnpc->Location;
+            const auto &nl = thisnpc->Location;
+
             MemAssign(thisnpc, offset, value, operation, ftype);
+
+            // Update tree if got altered
+            if(ol.X != nl.X || ol.Y != nl.Y || ol.Width != nl.Width || ol.Height != nl.Height)
+                treeNPCUpdate(i);
+        }
     }//for
 }
 
