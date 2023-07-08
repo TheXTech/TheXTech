45de7dfd35c3555e74bd6449fb241a17dfff267e
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2023-07-08 11:17:54 -0400

    render_gl.h: next try for value-initialization

--- src/core/opengl/render_gl.h
+++ src/core/opengl/render_gl.h
@@ -336,10 +336,12 @@ private:
 
     // framebuffer object (FBO) state
 
+    // note: {} value-initializes array to zeros
+
     // textures for each render-to-texture layer
-    std::array<GLuint, BUFFER_MAX> m_buffer_texture = std::array<GLuint, BUFFER_MAX>();
+    std::array<GLuint, BUFFER_MAX> m_buffer_texture{};
     // FBOs for each render-to-texture layer (only the first is required)
-    std::array<GLuint, BUFFER_MAX> m_buffer_fb = std::array<GLuint, BUFFER_MAX>();
+    std::array<GLuint, BUFFER_MAX> m_buffer_fb{};
     // texture for main game framebuffer's depth component (preferred)
     GLuint m_game_depth_texture = 0;
     // fallback: renderbuffer for main game framebuffer's depth component
