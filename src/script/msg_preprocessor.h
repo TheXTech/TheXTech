f9ba76bcc02259e740e2974c599fe1bc91261790
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2023-08-24 07:10:52 +0300

    Simple pre-processor for message boxes (#595)
    * Initial pre-processor implementation
    
    * msg_preprocessor.cpp: remove local player tracking
    
    * Move Preprocessor into "script" sub-directory
    
    Basically, such preprocessor is a kind of primitive scripting, and it seems better place to store it at "script" sub-directory
    
    * Update changelog.txt
    
    * Update changelog.txt
    
    Fix typo
    
    ---------
    
    Co-authored-by: ds-sloth <72112344+ds-sloth@users.noreply.github.com>
    Co-authored-by: 迎春心情 <37089530+LoveBodhi@users.noreply.github.com>

---
+++ src/script/msg_preprocessor.h
@@ -0,0 +1,34 @@
+/*
+ * TheXTech - A platform game engine ported from old source code for VB6
+ *
+ * Copyright (c) 2009-2011 Andrew Spinks, original VB6 code
+ * Copyright (c) 2020-2023 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef MSG_PREPROCESSOR_H
+#define MSG_PREPROCESSOR_H
+
+#include <string>
+
+/**
+ * @brief Pre-Process the message (apply conditions, or place player names where is possible)
+ * @param text Input-Output text to pre-process
+ * @param playerWho The player number who triggered this message box, or -1 to heuristically detect somebody.
+ */
+extern void preProcessMessage(std::string &text, int playerWho);
+
+#endif // MSG_PREPROCESSOR_H
