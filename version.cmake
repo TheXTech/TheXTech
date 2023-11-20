8a3653c92c9a065ed39cccc2c3b1cf5ddae0c135
Wohlstand <admin@wohlnet.ru>
2023-11-20 03:40:32 +0300

    Updated changelog and version for the 1.3.6.2

--- version.cmake
+++ version.cmake
@@ -7,10 +7,10 @@ set(THEXTECH_VERSION_3 6)
 # Patch
 set(THEXTECH_VERSION_4 2)
 # Type of version: "-alpha","-beta","-dev", or "" aka "release"
-set(THEXTECH_VERSION_REL "-dev")
+set(THEXTECH_VERSION_REL "")
 
 # Static version values for F-Droid to parse (PLEASE KEEP IT IN SYNC WITH VERSION NUMBER PARTS FROM ABOVE)
-set(THEXTECH_ANDROID_VERSION_NAME "1.3.6.2-dev")
+set(THEXTECH_ANDROID_VERSION_NAME "1.3.6.2")
 set(THEXTECH_ANDROID_VERSION_CODE "1030602")
 
 # Defining global macros
