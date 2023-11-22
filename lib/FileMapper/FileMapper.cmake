e034656945ff463710dfd55da98abd1962318d38
Wohlstand <admin@wohlnet.ru>
2023-11-22 04:04:45 +0300

    Turn the FileMapper into submodule
    This should make it being easily synced with Moondust Projects

--- lib/FileMapper/FileMapper.cmake
+++
@@ -1,9 +0,0 @@
-# message("Path to FileMapper is [${CMAKE_CURRENT_LIST_DIR}]")
-include_directories(${CMAKE_CURRENT_LIST_DIR})
-
-set(FILEMAPPER_SRCS)
-
-list(APPEND FILEMAPPER_SRCS
-    ${CMAKE_CURRENT_LIST_DIR}/file_mapper.cpp
-)
-
