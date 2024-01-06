TheXTech/TheXTech

commit 9589afe253dcd3a881bda33eafd7839a39d41d8c

setup_vars.cpp: reset all NPC type fields on SetupVars call

src/main/setup_vars.cpp
@@ -298,6 +298,51 @@ void SetupVars()
     EffectHeight[47] = 44;
     EffectWidth[47] = 44;
 
+    // NEW: reset all variables to their original values (it will be nice to convert all NPC traits to a single struct)
+    NPCFrameOffsetX.fill(0);
+    NPCFrameOffsetY.fill(0);
+    NPCWidth.fill(0);
+    NPCHeight.fill(0);
+    NPCWidthGFX.fill(0);
+    NPCHeightGFX.fill(0);
+    NPCSpeedvar.fill(0.f);
+
+    NPCIsAShell.fill(false);
+    NPCIsABlock.fill(false);
+    NPCIsAHit1Block.fill(false);
+    NPCIsABonus.fill(false);
+    NPCIsACoin.fill(false);
+    NPCIsAVine.fill(false);
+    NPCIsAnExit.fill(false);
+    NPCIsAParaTroopa.fill(false);
+    NPCIsCheep.fill(false);
+    NPCJumpHurt.fill(false);
+    NPCNoClipping.fill(false);
+    NPCScore.fill(0);
+    NPCCanWalkOn.fill(false);
+    NPCGrabFromTop.fill(false);
+    NPCTurnsAtCliffs.fill(false);
+    NPCWontHurt.fill(false);
+    NPCMovesPlayer.fill(false);
+    NPCStandsOnPlayer.fill(false);
+    NPCIsGrabbable.fill(false);
+    NPCIsBoot.fill(false);
+    NPCIsYoshi.fill(false);
+    NPCIsToad.fill(false);
+    NPCNoYoshi.fill(false);
+    NPCForeground.fill(false);
+    NPCIsABot.fill(false);
+    NPCDefaultMovement.fill(false);
+    NPCIsVeggie.fill(false);
+    NPCNoFireBall.fill(false);
+    NPCNoIceBall.fill(false);
+    NPCNoGravity.fill(false);
+
+    NPCFrame.fill(0);
+    NPCFrameSpeed.fill(0);
+    NPCFrameStyle.fill(0);
+
+    // SMBX64 logic:
     for(int A = 1; A <= maxNPCType; ++A)
     {
         NPCScore[A] = 2;

commit 3cc645fa553d338f438fb222d452485138ea6bbc

setup_vars.cpp: OUCH
Fixes bug introduced in 9589afe253dcd3a881bda33eafd7839a39d41d8c

src/main/setup_vars.cpp
@@ -305,7 +305,7 @@ void SetupVars()
     NPCHeight.fill(0);
     NPCWidthGFX.fill(0);
     NPCHeightGFX.fill(0);
-    NPCSpeedvar.fill(0.f);
+    NPCSpeedvar.fill(1.f);
 
     NPCIsAShell.fill(false);
     NPCIsABlock.fill(false);