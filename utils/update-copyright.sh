0b30d9031a73ce144ea0296bc0ba1ccd8ef94889
Wohlstand <admin@wohlnet.ru>
2024-01-07 04:42:10 +0300

    Update update-copyright.sh
    Added the condition to detect both "Vitaly" and "Vitaliy" Latin spellings of my name.

--- utils/update-copyright.sh
+++ utils/update-copyright.sh
@@ -4,7 +4,7 @@ find . -type f -exec grep -Il "Copyright" {} \;     \
 | grep -v \.git \
 | while read file;                            \
 do \
-  LC_ALL=C sed -b -i "s/\(.*Copyright.*\)[0-9]\{4\}\( *Vitaly Novichkov\)/\1`date +%Y`\2/" "$file"; \
+  LC_ALL=C sed -b -i "s/\(.*Copyright.*\)[0-9]\{4\}\( *Vitali\?y Novichkov\)/\1`date +%Y`\2/" "$file"; \
 done
 
 if [[ -f "pge_version.h" ]]; then
