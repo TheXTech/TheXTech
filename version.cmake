60c6374cf1ea33e56928d163c3ab597935f9c6c7
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-01-20 21:45:34 -0500

    Update version for 1.3.6.4

--- version.cmake
+++ version.cmake
@@ -5,13 +5,13 @@ set(THEXTECH_VERSION_2 3)
 # Revision
 set(THEXTECH_VERSION_3 6)
 # Patch
-set(THEXTECH_VERSION_4 3)
+set(THEXTECH_VERSION_4 4)
 # Type of version: "-alpha","-beta","-dev", or "" aka "release"
-set(THEXTECH_VERSION_REL "")
+set(THEXTECH_VERSION_REL "-dev")
 
 # Static version values for F-Droid to parse (PLEASE KEEP IT IN SYNC WITH VERSION NUMBER PARTS FROM ABOVE)
-set(THEXTECH_ANDROID_VERSION_NAME "1.3.6.3")
-set(THEXTECH_ANDROID_VERSION_CODE "1030603")
+set(THEXTECH_ANDROID_VERSION_NAME "1.3.6.4-dev")
+set(THEXTECH_ANDROID_VERSION_CODE "1030604")
 
 # Defining global macros
 add_definitions(-DTHEXTECH_VERSION_1=${THEXTECH_VERSION_1})
