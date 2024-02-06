b5c5e5c7a6b2988555b8ea4f62c4788c6fc20b13
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-02-06 16:11:45 -0500

    new_editor.cpp: use FreeS for level warp

--- src/editor/new_editor.cpp
+++ src/editor/new_editor.cpp
@@ -3778,7 +3778,7 @@ void EditorScreen::UpdateWarpScreen(CallMode mode)
             EditorCursor.Warp.MapWarp = !EditorCursor.Warp.MapWarp;
             if(EditorCursor.Warp.MapWarp)
             {
-                EditorCursor.Warp.level = STRINGINDEX_NONE;
+                FreeS(EditorCursor.Warp.level);
                 EditorCursor.Warp.LevelEnt = false;
             }
         }
@@ -3795,7 +3795,7 @@ void EditorScreen::UpdateWarpScreen(CallMode mode)
                 EditorCursor.Warp.LevelEnt = false;
             }
             else
-                EditorCursor.Warp.level = STRINGINDEX_NONE;
+                FreeS(EditorCursor.Warp.level);
         }
 
         SuperPrintRightR(mode, g_editorStrings.warpOut, 3, 360, 234);
@@ -3804,7 +3804,7 @@ void EditorScreen::UpdateWarpScreen(CallMode mode)
             EditorCursor.Warp.LevelEnt = !EditorCursor.Warp.LevelEnt;
             if(EditorCursor.Warp.LevelEnt)
             {
-                EditorCursor.Warp.level = STRINGINDEX_NONE;
+                FreeS(EditorCursor.Warp.level);
                 EditorCursor.Warp.MapWarp = false;
             }
         }
