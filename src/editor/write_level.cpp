aaf1bb7d961d8af76b70286825eec545fad3e2ed
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-02-06 15:37:09 -0500

    write_level.cpp: do save level name
    (cherry picked from commit 2afb23fe8bb6dfb27c3dbffc2c4677e39d0335ca)

--- src/editor/write_level.cpp
+++ src/editor/write_level.cpp
@@ -103,6 +103,10 @@ void SaveLevel(const std::string& FilePath, int format, int version)   // saves 
     //     FileNamePath = Left(FileNamePath, Len(FileNamePath) - 1)
     // End If
 
+    // Level-wide settings
+    out.LevelName = LevelName;
+
+    // sections
     for(int i = 0; i < numSections; ++i)
     {
         auto &s = level[i];
