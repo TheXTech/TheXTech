f29e14ab0dba5ea53cead7d33281745864601e56
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-03-14 11:29:03 -0400

    screen_connect.cpp: play SFX when player gets force-dropped

--- src/main/screen_connect.cpp
+++ src/main/screen_connect.cpp
@@ -2252,6 +2252,8 @@ int PlayerBox::Logic()
                 PlaySoundMenu(SFX_Slide);
                 return -1;
             }
+            else
+                PlaySoundMenu(SFX_PlayerDied2);
 
             m_menu_item = 0;
         }
