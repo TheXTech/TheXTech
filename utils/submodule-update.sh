d81290963d6d728d0b0e78fd207d259378dd459f
Wohlstand <admin@wohlnet.ru>
2024-04-02 18:34:19 +0300

    Bumped submodules

--- utils/submodule-update.sh
+++ utils/submodule-update.sh
@@ -7,6 +7,10 @@ if [[ "$reponame" == "LuaJIT" ]]; then
     echo "Branch v2.1"
     git checkout v2.1
     git pull origin v2.1
+elif [[ "$reponame" == "angle-shader-translator" ]]; then
+    echo "Branch dist-no-spirv"
+    git checkout dist-no-spirv
+    git pull origin dist-no-spirv
 elif [[ "$reponame" == "harfbuzz" || "$reponame" == "thextech-discord-rpc" ]]; then
     echo "Branch main"
     git checkout main
