TheXTech/TheXTech

commit f2a00bf3812cb53d4de6af44d363a55525ddbfa7

level_file.cpp: remove PGE-FL preprocessing
This preprocessing isn't needed in SMBX 1.3, and TheXTech should now be robust to block and BGO order

src/main/level_file.cpp
@@ -218,10 +218,6 @@ bool OpenLevelData(LevelData &lvl, const std::string FilePath)
         // none supported yet
     }
 
-    FileFormats::smbx64LevelPrepare(lvl);
-    FileFormats::smbx64LevelSortBlocks(lvl);
-    FileFormats::smbx64LevelSortBGOs(lvl);
-
     numBlock = 0;
     numBackground = 0;
     numLocked = 0;

commit f6647e9ee232abaf7a2b3bc9bfa74a4125d67f5b

level_file.cpp: do sort content on load if saved in LVLX format
Fixes failure to load some Moondust-saved content introduced in f2a00bf3

src/main/level_file.cpp
@@ -219,6 +219,14 @@ bool OpenLevelData(LevelData &lvl, const std::string FilePath)
         // none supported yet
     }
 
+    // FIXME: disable this if the file indicates that it is already sorted
+    if(g_compatibility.emulate_classic_block_order && FileFormat == FileFormats::LVL_PGEX)
+    {
+        FileFormats::smbx64LevelPrepare(lvl);
+        FileFormats::smbx64LevelSortBlocks(lvl);
+        FileFormats::smbx64LevelSortBGOs(lvl);
+    }
+
     numBlock = 0;
     numBackground = 0;
     numLocked = 0;