f134abf8f8166748e818b5917da4efafbd511dd9
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-06-20 16:12:54 -0400

    screen_connect.cpp: clean up some experiments

--- src/main/screen_connect.cpp
+++ src/main/screen_connect.cpp
@@ -13,7 +13,6 @@
 #include "npc_id.h"
 #include "npc/npc_queues.h"
 
-#include "graphics/gfx_frame.h"
 #include "graphics/gfx_marquee.h"
 
 #include "main/game_info.h"
@@ -1968,7 +1967,6 @@ int Mouse_Render(bool mouse, bool render)
     {
         if(render)
         {
-            // DrawSimpleFrame(start_x, draw_y, full_width, full_height, {0, 0, 0, 127}, {255, 255, 255, 127}, {0, 0, 0, 127});
             XRender::renderRect(start_x, draw_y, full_width, full_height, XTColorF(0.0f, 0.0f, 0.0f, 0.5f));
             SuperPrintScreenCenter(g_gameStrings.pauseItemPlayerSetup, 3, draw_y + 8);
         }
