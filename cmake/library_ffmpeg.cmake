76ef501a4625355327ad47f28da866a483345083
Wohlstand <admin@wohlnet.ru>
2024-06-26 06:09:20 +0300

    Initial ffmpeg building script
    It hadn't been integrated properly into the build. Yet. Additionally it's need to properly detect build dependencies to properly build it from the sources at the and for the target platform (if the cross-compilation is).

---
+++ cmake/library_ffmpeg.cmake
@@ -0,0 +1,120 @@
+option(FFMPEG_PIC "Enable -fPIC flag for FFMPEG" ON)
+
+add_library(PGE_FFMPEG INTERFACE)
+
+if(VITA OR NINTENDO_SWITCH OR NINTENDO_3DS OR NINTENDO_WII OR NINTENDO_WIIU)
+    set(FFMPEG_PIC OFF)
+endif()
+
+if(USE_SYSTEM_LIBS)
+    add_library(FFMPEG_Local INTERFACE)
+
+    find_path(AVCODEC_INCLUDE_DIR NAMES libavcodec/avcodec.h PATH_SUFFIXES ffmpeg)
+    find_library(AVCODEC_LIBRARY avcodec)
+
+    find_path(AVFORMAT_INCLUDE_DIR libavformat/avformat.h PATH_SUFFIXES ffmpeg)
+    find_library(AVFORMAT_LIBRARY avformat)
+
+    find_path(AVUTIL_INCLUDE_DIR libavutil/avutil.h PATH_SUFFIXES ffmpeg)
+    find_library(AVUTIL_LIBRARY avutil)
+
+    find_path(SWSCALE_INCLUDE_DIR libswscale/swscale.h PATH_SUFFIXES ffmpeg)
+    find_library(SWSCALE_LIBRARY swscale)
+
+    find_path(SWRESAMPLE_INCLUDE_DIR libswresample/swresample.h PATH_SUFFIXES ffmpeg)
+    find_library(SWRESAMPLE_LIBRARY swresample)
+
+    if(AVCODEC_INCLUDE_DIR AND AVCODEC_LIBRARY AND AVFORMAT_INCLUDE_DIR AND AVFORMAT_LIBRARY AND SWSCALE_INCLUDE_DIR AND SWSCALE_LIBRARY AND SWRESAMPLE_INCLUDE_DIR AND SWRESAMPLE_LIBRARY)
+        target_link_libraries(PGE_FFMPEG INTERFACE ${AVCODEC_LIBRARY} ${AVFORMAT_LIBRARY} ${AVUTIL_LIBRARY} ${SWSCALE_LIBRARY} ${SWRESAMPLE_LIBRARY})
+        target_include_directories(PGE_FFMPEG INTERFACE ${AVCODEC_INCLUDE_DIR} ${AVFORMAT_INCLUDE_DIR} ${AVUTIL_INCLUDE_DIR} ${SWSCALE_INCLUDE_DIR} ${SWRESAMPLE_INCLUDE_DIR})
+    endif()
+
+    set(FFMPEG_Libs ${AVCODEC_LIBRARY} ${AVFORMAT_LIBRARY} ${AVUTIL_LIBRARY} ${SWSCALE_LIBRARY} ${SWRESAMPLE_LIBRARY})
+else()
+    set(FFMPEG_Libs
+        "${DEPENDENCIES_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}swresamplemixerx${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        "${DEPENDENCIES_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avcodecmixerx${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        "${DEPENDENCIES_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avformatmixerx${CMAKE_STATIC_LIBRARY_SUFFIX}"
+        "${DEPENDENCIES_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}avutilmixerx${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    )
+
+    if(CMAKE_BUILD_TYPE_LOWER STREQUAL "debug")
+        set(FFMPEG_DEBUG_ARGS --enable-debug)
+    else()
+        set(FFMPEG_DEBUG_ARGS --enable-small --disable-debug --enable-stripping)
+    endif()
+
+    if(FFMPEG_PIC)
+        set(FFMPEG_PIC_ARGS --enable-pic --extra-cflags=-fPIC --extra-cxxflags=-fPIC)
+    else()
+        set(FFMPEG_PIC_ARGS --disable-pic)
+    endif()
+
+    if(PGE_ENABLE_VIDEO_REC AND NOT PGE_VIDEO_REC_PREFER_GIF)
+        set(FFMPEG_VPX_ARGS --enable-libvpx --enable-encoder=opus,libvpx_vp8,libvpx_vp9)
+        set(FFMPEG_DEPENDS LIBVPX_Local)
+    endif()
+
+    if("${TARGET_PROCESSOR}" STREQUAL "ppc")
+        set(FFMPEG_ARCH_ARGS --disable-altivec --disable-vsx --disable-power8 --disable-ppc4xx --disable-dcbzl)
+    endif()
+
+
+    ExternalProject_Add(
+        FFMPEG_Local
+        PREFIX ${CMAKE_BINARY_DIR}/external/ffmpeg
+        DOWNLOAD_COMMAND ""
+        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../3rdparty/ffmpeg
+        CONFIGURE_COMMAND
+            "${CMAKE_CURRENT_LIST_DIR}/../3rdparty/ffmpeg/configure"
+            "--prefix=${DEPENDENCIES_INSTALL_DIR}"
+            --enable-rpath
+            --build-suffix=mixerx
+            --toolchain=hardened
+            --disable-all
+            --disable-autodetect
+            --disable-hwaccels
+            --disable-asm
+            --disable-x86asm
+            --disable-network
+            --disable-doc
+            --disable-iconv
+            --disable-sdl2
+
+            ${FFMPEG_ARCH_ARGS}
+
+            --disable-bzlib
+            --disable-zlib
+            --enable-static --disable-pthreads
+
+            --disable-cuvid --disable-d3d11va --disable-dxva2 --disable-ffnvcodec --disable-vaapi --disable-vdpau
+
+            --enable-avcodec --enable-avformat --enable-swresample
+
+            --enable-decoder=wmav1,wmav2,wmavoice,wmapro,wmalossless,aac,opus
+            --enable-demuxer=asf,asf_o,aac,mov,matroska
+            --enable-parser=aac,mpegaudio
+            --extra-ldflags=-Wl,--no-undefined
+
+            ${FFMPEG_VPX_ARGS}
+            ${FFMPEG_PIC_ARGS}
+            ${FFMPEG_DEBUG_ARGS}
+
+        BUILD_COMMAND
+            make -j 2
+
+        INSTALL_COMMAND
+            make install
+
+        DEPENDS
+            ${FFMPEG_DEPENDS}
+
+        BUILD_BYPRODUCTS
+            "${FFMPEG_Libs}"
+    )
+
+    message("-- FFMPEG will be built: ${FFMPEG_Libs} --")
+
+    target_link_libraries(PGE_FFMPEG INTERFACE "${FFMPEG_Libs}")
+    target_include_directories(PGE_FFMPEG INTERFACE "${DEPENDENCIES_INSTALL_DIR}/include/")
+endif()
---
+++ cmake/library_libvpx.cmake
@@ -0,0 +1,87 @@
+option(LIBVPX_PIC "Enable -fPIC flag for libVPX" ON)
+
+add_library(PGE_libVPX INTERFACE)
+
+if(VITA OR NINTENDO_SWITCH OR NINTENDO_3DS OR NINTENDO_WII OR NINTENDO_WIIU)
+    set(LIBVPX_PIC OFF)
+endif()
+
+if(USE_SYSTEM_LIBS)
+    add_library(LIBVPX_Local INTERFACE)
+
+    find_package(LIBVPX REQUIRED)
+    message("-- Found libVPX: ${LIBVPX_LIBRARIES} --")
+    target_link_libraries(PGE_libVPX INTERFACE ${LIBVPX_LIBRARIES})
+    target_include_directories(PGE_libVPX INTERFACE ${LIBVPX_INCLUDE_DIRS})
+
+    set(libVPX_Libs "${LIBVPX_LIBRARIES}")
+else()
+    set(libVPX_Libs "${DEPENDENCIES_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}vpx${CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+    if(CMAKE_BUILD_TYPE_LOWER STREQUAL "debug")
+        set(LIBVPX_DEBUG_ARGS --enable-debug)
+    endif()
+
+    if(LIBVPX_PIC)
+        set(LIBVPX_PICARGS --enable-pic)
+    else()
+        set(LIBVPX_PICARGS --disable-pic)
+    endif()
+
+    ExternalProject_Add(
+        LIBVPX_Local
+        PREFIX ${CMAKE_BINARY_DIR}/external/libvpx
+        DOWNLOAD_COMMAND ""
+        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../3rdparty/libvpx
+        CONFIGURE_COMMAND
+            "${CMAKE_CURRENT_LIST_DIR}/../3rdparty/libvpx/configure"
+            "--prefix=${DEPENDENCIES_INSTALL_DIR}"
+            --disable-examples
+            --disable-tools
+            --disable-docs
+            --disable-unit-tests
+            ${LIBVPX_PICARGS}
+            ${LIBVPX_DEBUG_ARGS}
+
+        BUILD_COMMAND
+            make -j 2
+
+        INSTALL_COMMAND
+            make install
+
+#         CMAKE_ARGS
+#             "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+#             "-DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_INSTALL_DIR}"
+#             "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+#             "-DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}"
+# #            "-DCMAKE_CONFIGURATION_TYPES=${CMAKE_CONFIGURATION_TYPES}"
+#             "-DCMAKE_POSITION_INDEPENDENT_CODE=${FREETYPE_PIC}"
+#             "-DDISABLE_FORCE_DEBUG_POSTFIX=ON"
+#             "-DCMAKE_DEBUG_POSTFIX=${PGE_LIBS_DEBUG_SUFFIX}"
+#             ${ANDROID_CMAKE_FLAGS}
+#             -DFT_ENABLE_ERROR_STRINGS=ON
+#             -DFT_DISABLE_ZLIB=OFF
+#             -DFT_DISABLE_BZIP2=ON
+#             -DFT_DISABLE_BROTLI=ON
+#             -DFT_DISABLE_PNG=ON
+#             -DFT_DISABLE_BROTLI=ON
+#             "-DZLIB_INCLUDE_DIR=${FREETYPE_ZLIB_INCLUDE}"
+#             "-DZLIB_LIBRARY=${AC_ZLIB}"
+#             -DFT_DISABLE_HARFBUZZ=${FT_DISABLE_HARFBUZZ}
+#             "-DHARFBUZZ_LIBRARIES=${libHarfBuzz_Libs}"
+#             -DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE
+#             -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE
+#             -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE
+#             $<$<BOOL:APPLE>:-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}>
+#             $<$<BOOL:APPLE>:-DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}>
+
+        BUILD_BYPRODUCTS
+            "${libVPX_Libs}"
+    )
+
+
+    message("-- libVPX will be built: ${libVPX_Libs} --")
+
+    target_link_libraries(PGE_libVPX INTERFACE "${libVPX_Libs}")
+    target_include_directories(PGE_libVPX INTERFACE "${DEPENDENCIES_INSTALL_DIR}/include/vpx")
+endif()
