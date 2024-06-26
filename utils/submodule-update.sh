b83ced3beea274d50e7bdba2171fe35ffc7fe917
Wohlstand <admin@wohlnet.ru>
2024-06-26 04:31:06 +0300

    Added ffmpeg and libvpx as as submodules

--- utils/submodule-update.sh
+++ utils/submodule-update.sh
@@ -11,6 +11,14 @@ elif [[ "$reponame" == "angle-shader-translator" ]]; then
     echo "Branch dist-no-spirv"
     git checkout dist-no-spirv
     git pull origin dist-no-spirv
+elif [[ "$reponame" == "libvpx" ]]; then
+    echo "Branch stable-moondust"
+    git checkout stable-moondust
+    git pull origin stable-moondust
+elif [[ "$reponame" == "ffmpeg" ]]; then
+    echo "Branch release/7.0"
+    git checkout "release/7.0"
+    git pull origin "release/7.0"
 elif [[ "$reponame" == "harfbuzz" || "$reponame" == "thextech-discord-rpc" ]]; then
     echo "Branch main"
     git checkout main
