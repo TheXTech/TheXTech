e9d2c3c9f2128ff7e0747cb9b411d1091e9922d9
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-06-28 18:34:31 -0400

    Don't play sound on offscreen death for plants (#470)

--- src/npc/npc_hit.cpp
+++ src/npc/npc_hit.cpp
@@ -1690,8 +1690,9 @@ void NPCHit(int A, int B, int C)
     {
         if(!(B == 1 || B == 2 || B == 6))
         {
-            if(B != 8 && B != 10)
+            if(B != 8 && B != 10 && !(B == 9 && g_config.fix_visual_bugs))
                 PlaySoundSpatial(SFX_ShellHit, NPC[A].Location);
+
             NPC[A].Killed = B;
         }
     }
