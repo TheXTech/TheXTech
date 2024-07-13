1378e2ef65b6a8f369c7e13bf950de719b1639c1
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-07-13 13:49:55 -0400

    CMakeLists.txt: Add build shim for RWops

---
+++ lib/sdl_proxy_rwops/include/SDL2/SDL_rwops.h
@@ -0,0 +1,91 @@
+/*
+ * TheXTech - A platform game engine ported from old source code for VB6
+ *
+ * Copyright (c) 2009-2011 Andrew Spinks, original VB6 code
+ * Copyright (c) 2020-2024 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef SDL_PROXY_RWOPS_H
+#define SDL_PROXY_RWOPS_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* RWops Types */
+#define SDL_RWOPS_UNKNOWN   0U  /**< Unknown stream type */
+#define SDL_RWOPS_STDFILE   2U  /**< Stdio file */
+
+#define RW_SEEK_CUR SEEK_CUR
+#define RW_SEEK_SET SEEK_SET
+#define RW_SEEK_END SEEK_END
+
+typedef struct SDL_RWops
+{
+    /**
+     *  Return the size of the file in this rwops, or -1 if unknown
+     */
+    int64_t (* size) (struct SDL_RWops * context);
+
+    /**
+     *  Seek to `offset` relative to `whence`, one of stdio's whence values:
+     *  RW_SEEK_SET, RW_SEEK_CUR, RW_SEEK_END
+     *
+     *  \return the final offset in the data stream, or -1 on error.
+     */
+    int64_t (* seek) (struct SDL_RWops * context, int64_t offset,
+                             int whence);
+
+    /**
+     *  Read up to `maxnum` objects each of size `size` from the data
+     *  stream to the area pointed at by `ptr`.
+     *
+     *  \return the number of objects read, or 0 at error or end of file.
+     */
+    int64_t (* read) (struct SDL_RWops * context, void *ptr,
+                             size_t size, size_t maxnum);
+
+    /**
+     *  Write exactly `num` objects each of size `size` from the area
+     *  pointed at by `ptr` to data stream.
+     *
+     *  \return the number of objects written, or 0 at error or end of file.
+     */
+    int64_t (* write) (struct SDL_RWops * context, const void *ptr,
+                              size_t size, size_t num);
+
+    /**
+     *  Close and free an allocated SDL_RWops structure.
+     *
+     *  \return 0 if successful or -1 on write error when flushing data.
+     */
+    int (* close) (struct SDL_RWops * context);
+
+    int type;
+
+    void* hidden;
+} SDL_RWops;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* #ifndef SDL_PROXY_RWOPS_H */
