68cf7881c77b7c70d28382b415843b01974747a5
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-07-25 09:24:31 -0400

    json.hpp: split rwops patch into own header

--- lib/json/json.hpp
+++ lib/json/json.hpp
@@ -6201,49 +6201,6 @@ class input_stream_adapter
     std::istream* is = nullptr;
     std::streambuf* sb = nullptr;
 };
-
-static constexpr size_t s_rwops_input_adapter_buffer_size = 2048;
-
-/*!
-Input adapter for SDL_RWops file access. Uses a 2KB cache.
-*/
-class rwops_input_adapter
-{
-  public:
-    using char_type = uint8_t;
-
-    explicit rwops_input_adapter(SDL_RWops* rwops) noexcept
-        : m_rwops(rwops) {}
-
-    // make class move-only
-    rwops_input_adapter(const rwops_input_adapter&) = delete;
-    rwops_input_adapter(rwops_input_adapter&&) noexcept = default;
-    rwops_input_adapter& operator=(const rwops_input_adapter&) = delete;
-    rwops_input_adapter& operator=(rwops_input_adapter&&) = delete;
-    ~rwops_input_adapter() = default;
-
-    std::char_traits<uint8_t>::int_type get_character() noexcept
-    {
-        if(m_temp_buf_cur >= m_temp_buf_end)
-        {
-            m_temp_buf_cur = 0;
-            m_temp_buf_end = SDL_RWread(m_rwops, &m_temp_buf[0], 1, sizeof(m_temp_buf));
-
-            if(m_temp_buf_end == 0)
-                return std::char_traits<uint8_t>::eof();
-        }
-
-        return m_temp_buf[m_temp_buf_cur++];
-    }
-
-  private:
-    /// the rwops stream to read from
-    SDL_RWops* m_rwops;
-
-    uint8_t m_temp_buf[s_rwops_input_adapter_buffer_size];
-    int m_temp_buf_cur = 0;
-    int m_temp_buf_end = 0;
-};
 #endif  // JSON_NO_IO
 
 // General-purpose iterator-based adapter. It might not be as fast as
@@ -6548,10 +6505,8 @@ inline input_stream_adapter input_adapter(std::istream&& stream)
     return input_stream_adapter(stream);
 }
 
-inline rwops_input_adapter input_adapter(SDL_RWops* rwops)
-{
-    return rwops_input_adapter(rwops);
-}
+#include "_priv_rwops.patch.hpp"
+
 #endif  // JSON_NO_IO
 
 using contiguous_bytes_input_adapter = decltype(input_adapter(std::declval<const char*>(), std::declval<const char*>()));
