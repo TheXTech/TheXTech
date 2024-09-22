014f1c6b6e929add81ec9fd2afc1a8530121fbbe
Wohlstand <Wohlstand@users.noreply.github.com>
2024-09-23 01:25:35 +0300

    Updated JSON and fixed the char_traits warnings

--- lib/json/json_rwops_input.hpp
+++ lib/json/json_rwops_input.hpp
@@ -16,7 +16,7 @@ namespace nlohmann
 #if NLOHMANN_JSON_NAMESPACE_NO_VERSION
 inline namespace json_abi
 #else
-inline namespace json_abi_v3_11_2
+inline namespace json_abi_v3_11_3
 #endif
 {
 namespace detail
@@ -33,6 +33,35 @@ class rwops_input_adapter
 public:
     using char_type = uint8_t;
 
+    // Primary template of char_traits calls std char_traits
+    template<typename T>
+    struct char_traits : std::char_traits<T>
+    {};
+
+    // Explicitly define char traits for unsigned char since it is not standard
+    template<>
+    struct char_traits<unsigned char> : std::char_traits<char>
+    {
+        using char_type = unsigned char;
+        using int_type = uint64_t;
+
+        // Redefine to_int_type function
+        static int_type to_int_type(char_type c) noexcept
+        {
+            return static_cast<int_type>(c);
+        }
+
+        static char_type to_char_type(int_type i) noexcept
+        {
+            return static_cast<char_type>(i);
+        }
+
+        static constexpr int_type eof() noexcept
+        {
+            return static_cast<int_type>(EOF);
+        }
+    };
+
     explicit rwops_input_adapter(SDL_RWops* rwops) noexcept
         : m_rwops(rwops) {}
 
@@ -43,7 +72,7 @@ public:
     rwops_input_adapter& operator=(rwops_input_adapter&&) = delete;
     ~rwops_input_adapter() = default;
 
-    std::char_traits<uint8_t>::int_type get_character() noexcept
+    char_traits<uint8_t>::int_type get_character() noexcept
     {
         if(m_temp_buf_cur >= m_temp_buf_end)
         {
@@ -51,7 +80,7 @@ public:
             m_temp_buf_end = SDL_RWread(m_rwops, &m_temp_buf[0], 1, sizeof(m_temp_buf));
 
             if(m_temp_buf_end == 0)
-                return std::char_traits<uint8_t>::eof();
+                char_traits<uint8_t>::eof();
         }
 
         return m_temp_buf[m_temp_buf_cur++];
