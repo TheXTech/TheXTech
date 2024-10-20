498f16027654503d7394d6baf4d0f875698b2f74
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2024-10-20 16:29:45 -0400

    gfx.cpp: don't use a textbox on load failure (#840)

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -177,11 +177,11 @@ bool GFX_t::load()
 
     if(m_loadErrors > 0)
     {
-        std::string msg = fmt::format_ne("Failed to load UI image assets from {0}. Look a log file to get more details:\n{1}"
-                                         "\n\n"
-                                         "It's possible that you didn't install the game assets package, or you had installed it at the incorrect directory.",
-                                         AppPath, getLogFilePath());
-        XMsgBox::simpleMsgBox(XMsgBox::MESSAGEBOX_ERROR, "UI image assets loading error", msg);
+        // std::string msg = fmt::format_ne("Failed to load UI image assets from {0}. Look a log file to get more details:\n{1}"
+        //                                  "\n\n"
+        //                                  "It's possible that you didn't install the game assets package, or you had installed it at the incorrect directory.",
+        //                                  AppPath, getLogFilePath());
+        // XMsgBox::simpleMsgBox(XMsgBox::MESSAGEBOX_ERROR, "UI image assets loading error", msg);
         m_loadErrors = 0;
         return false;
     }
