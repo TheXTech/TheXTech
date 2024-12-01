TheXTech/TheXTech

commit ffea940b53c869e3f430cfaa3831fb47d3a2cbf6

Implemented the in-game assert failure message. Attempt two. (#862)
* Implemented the in-game assert failure message

On platforms that doesn't have SDL's message box implemented, show the in-game assertion error message until close the game. Debugged primarily on Wii U, need to test on Vita, 3DS, Switch, and Wii.

* Clean up fatal message code

* gfx_message.cpp: remove debug text

* sound.cpp: Added missing guard of PlayErrorSound()

* sound.cpp: Extended condition of built-in fallback

* Ouch

Condition here makes an evil!

* Added experimental hook for signals

However, it doesn't work on Wii U, just because Wii U app when crashes, just stucks.

* Make signals catching work conditionally

- Don't try to catch signals on 3DS and Wii: only C++ exceptions and on memory flood

* Adjusted C++ exception message box

* crash_handler.cpp: polish C++ exception text

* crash_handler.cpp: Remove all Qt Translator comments

They are just unused here as this project doesn't use Qt Translator at all (and, probably, Moondust Engine itself will use custom translation system instead of Qt Translator that will be kept for devkit tools that depends on Qt)

* Whoops

Forgot to commit this too

* game_loop.cpp: Don't call abort() on Wii too

* Crash handler: disable signals for Vita too

They has no effect, the system-wide signals handler works instead

* Fixed Switch assert/exception handler didn't worked

---------

Co-authored-by: ds-sloth <72112344+ds-sloth@users.noreply.github.com>

src/sound.cpp
@@ -1314,7 +1314,10 @@ void InitSound()
     }
 
     if(LoadingInProcess)
+    {
+        LoaderUpdateDebugString("All sounds loaded");
         UpdateLoad();
+    }
 
     Mix_ReserveChannels(g_reservedChannels);
 
@@ -1514,13 +1517,26 @@ bool HasSound(int A)
 
 void PlaySoundMenu(int A, int loops)
 {
-    if(SoundPause[A] == 0) // if the sound wasn't just played
+    if(SoundPause[A] > 0) // if the sound wasn't just played
+        return;
+
+    if(g_totalSounds == 0 || A > (int)g_totalSounds)
     {
-        std::string alias = fmt::format_ne("sound{0}", A);
-        PlaySfx(alias, loops);
-        s_resetSoundDelay(A);
+        playFallbackSfx(A, loops, 128);
+        return;
     }
+
+    std::string alias = fmt::format_ne("sound{0}", A);
+    PlaySfx(alias, loops);
+    s_resetSoundDelay(A);
 }
+
+#if defined(THEXTECH_ASSERTS_INGAME_MESSAGE) && !defined(THEXTECH_NO_SDL_BUILD)
+void PlayErrorSound(int A, int loops)
+{
+    playFallbackSfx(A, loops, 128);
+}
+#endif
 
 // stops all sound from being played for 10 cycles
 void BlockSound()

CMakeLists.txt
@@ -1311,7 +1311,12 @@ elseif(PGE_MIN_PORT)
     target_compile_definitions(thextech PRIVATE -DLOW_MEM)
 endif()
 
-if(NINTENDO_DS OR NINTENDO_3DS OR NINTENDO_WII OR NINTENDO_SWITCH OR ANDROID OR EMSCRIPTEN)
+if(VITA OR NINTENDO_3DS OR NINTENDO_WII OR NINTENDO_WIIU OR NINTENDO_SWITCH)
+    # Run in-game message box to show assert failures on platforms without SDL's message box
+    target_compile_definitions(thextech PRIVATE -DTHEXTECH_ASSERTS_INGAME_MESSAGE)
+endif()
+
+if(VITA OR NINTENDO_DS OR NINTENDO_3DS OR NINTENDO_WII OR NINTENDO_SWITCH OR ANDROID OR EMSCRIPTEN)
     # Disable language tools at non-desktop platforms
     target_compile_definitions(thextech PRIVATE -DTHEXTECH_DISABLE_LANG_TOOLS)
 endif()

lib/CrashHandler/crash_handler.cpp
@@ -24,11 +24,14 @@
 #   include "sdl_proxy/sdl_assert.h"
 #endif
 
-#ifndef THEXTECH_NO_SDL_BUILD
-#include <SDL2/SDL_version.h>
+#if !defined(__16M__) && !defined(__WII__) && !defined(__3DS__) && !defined(__SWITCH__) && !defined(VITA)
+#   define PGE_ENABLE_SIGNAL_HOOKS
+#endif
 
+#ifndef THEXTECH_NO_SDL_BUILD
+#   include <SDL2/SDL_version.h>
 #   if !defined(THEXTECH_CLI_BUILD) && !defined(CUSTOM_AUDIO)
-#   include <SDL2/SDL_mixer_ext.h>
+#       include <SDL2/SDL_mixer_ext.h>
 #   endif
 #endif
 
@@ -39,7 +42,7 @@
 #include <lib/CrashHandler/backtrace.h>
 
 #if defined(DEBUG_BUILD) && (Backtrace_FOUND || defined(_WIN32))
-#define PGE_ENGINE_DEBUG
+#   define PGE_ENGINE_DEBUG
 #endif
 
 #ifdef PGE_ENGINE_DEBUG
@@ -66,6 +69,7 @@
 
 // Exclude platforms that don't have SIG_INFO support
 #if    !defined(_WIN32) \
+    && !defined(__16M__) \
     && !defined(__3DS__) \
     && !defined(__WII__) \
     && !defined(__WIIU__) \
@@ -76,6 +80,7 @@
 
 // Exclude personal data removal from platforms where API doesn't allows to recognise the user and/or home directory
 #if    !defined(VITA) \
+    && !defined(__16M__) \
     && !defined(__3DS__) \
     && !defined(__WII__) \
     && !defined(__WIIU__)  \
@@ -402,14 +407,20 @@ int ppc_backtrace(void **buffer, int size)
     int depth;
     uint32_t stackptr = 0, lr, *addr;
 
+    pLogDebug("Stack trace: PPC: Get link register...");
+
     // get link register
     asm volatile ("mflr %0" : "=r"(lr));
 
     // link register is assigned to depth[0]
     buffer[0] = (void *) (lr - 4);
 
+    pLogDebug("Stack trace: PPC: Get link stack pointer...");
+
     // get stackpointer
     asm volatile("stw %%sp, 0(%0)" : : "b" ((uint32_t)&stackptr));
+
+    pLogDebug("Stack trace: PPC: Assign...");
 
     // assign stack ptr to address
     addr = reinterpret_cast<uint32_t *>(stackptr);
@@ -426,6 +437,8 @@ int ppc_backtrace(void **buffer, int size)
 
     }
 
+    D_pLogDebugNA("Stack trace: PPC: Loop...");
+
     for(depth = 1; (depth < size && *addr); ++depth)
     {
         uint32_t * next = (uint32_t *) *addr;
@@ -433,34 +446,40 @@ int ppc_backtrace(void **buffer, int size)
         addr = next;
     }
 
+    D_pLogDebugNA("Stack trace: PPC: Return...");
+
     return depth;
 }
 #endif
 
 static std::string getStacktrace()
 {
-    D_pLogDebugNA("Initializing std::string...");
+    D_pLogDebugNA("Stack trace: Initializing std::string...");
     std::string bkTrace;
+    D_pLogDebugNA("Stack trace: String done...");
 
 #if defined(_WIN32)
     GetStackWalk(bkTrace);
 
 #elif defined(USE_PPC_BACKTRACE)
+
+    D_pLogDebugNA("Stack trace: Static arrays...");
     void  *array[400];
     char stack_entry[25];
     int size;
+    D_pLogDebugNA("Stack trace: Done...");
 
-    D_pLogDebugNA("Requesting backtrace...");
+    D_pLogDebugNA("Stack trace: Requesting backtrace...");
     size = ppc_backtrace(array, 400);
 
-    D_pLogDebugNA("Filling std::string...");
+    D_pLogDebugNA("Stack trace: Filling std::string...");
     for(int j = 0; j < size; j++)
     {
         SDL_snprintf(stack_entry, 25, "- 0x%08x\n", (uint32_t)array[j]);
         bkTrace.append(stack_entry);
     }
 
-    D_pLogDebugNA("DONE!");
+    D_pLogDebugNA("Stack trace: DONE!");
 
 #elif Backtrace_FOUND
     void  *array[400];
@@ -509,19 +528,28 @@ static std::string getStacktrace()
 #   define LLVM_ATTRIBUTE_NORETURN
 #endif
 
-static LLVM_ATTRIBUTE_NORETURN void abortEngine(int signal)
+static LLVM_ATTRIBUTE_NORETURN void abortEngine(int sig)
 {
     CloseLog();
+    signal(SIGABRT, SIG_DFL);
 #ifndef THEXTECH_NO_SDL_BUILD
     SDL_Quit();
 #endif
-    exit(signal);
+    exit(sig);
+}
+
+static CrashHandler::MsgBoxHook_t g_msgBoxHook = &XMsgBox::errorMsgBox;
+
+void CrashHandler::setCrashMsgBoxHook(MsgBoxHook_t hook)
+{
+    g_msgBoxHook = hook;
 }
 
 void LLVM_ATTRIBUTE_NORETURN CrashHandler::crashByUnhandledException()
 {
     std::string stack = getStacktrace();
     std::string exc;
+    std::string exc_line;
 
     try
     {
@@ -529,23 +557,27 @@ void LLVM_ATTRIBUTE_NORETURN CrashHandler::crashByUnhandledException()
     }
     catch(const std::exception &e)
     {
+        exc_line = e.what();
         exc.append(" caught an unhandled exception. [");
-        exc.append(e.what());
+        exc.append(exc_line);
         exc.append("]");
     }
     catch(...)
     {
+        exc_line = "<unknown>";
         exc.append(" caught unhandled exception. (unknown) ");
     }
 
     pLogFatal("<Unhandled exception! %s>\n"
               STACK_FORMAT, exc.c_str(),
               stack.c_str(), g_messageToUser);
-    XMsgBox::errorMsgBox(
-        //% "Unhandled exception!"
+    g_msgBoxHook(
         "Unhandled exception!",
-        //% "Engine has crashed because accepted unhandled exception!"
-        "Engine has crashed because accepted unhandled exception!");
+        "The engine has crashed because of an unhandled exception!\n\n"
+        "--Exception message:----\n" +
+        exc_line + "\n"
+        "------------------------"
+    );
     abortEngine(-1);
 }
 
@@ -555,14 +587,13 @@ void LLVM_ATTRIBUTE_NORETURN CrashHandler::crashByFlood()
     pLogFatal("<Out of memory!>\n"
               STACK_FORMAT,
               stack.c_str(), g_messageToUser);
-    XMsgBox::errorMsgBox(
-        //% "Out of memory!"
+    g_msgBoxHook(
         "Out of memory!",
-        //% "Engine has crashed because out of memory! Try to close other applications and restart game."
         "Engine has crashed because out of memory! Try to close other applications and restart game.");
     abortEngine(-2);
 }
 
+#if defined(PGE_ENABLE_SIGNAL_HOOKS)
 #if defined(_WIN32) // Unsupported signals by Windows
 struct siginfo_t;
 #endif
@@ -593,10 +624,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
     case SIGALRM:
     {
         pLogFatal("<alarm() time out!>");
-        XMsgBox::errorMsgBox(
-            //% "Time out!"
+        g_msgBoxHook(
             "Time out!",
-            //% "Engine has abourted because alarm() time out!"
             "Engine has abourted because alarm() time out!");
         abortEngine(signal);
     }
@@ -642,10 +671,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
                       stack.c_str(), g_messageToUser);
         }
 
-        XMsgBox::errorMsgBox(
-            //% "Physical memory address error!"
+        g_msgBoxHook(
             "Physical memory address error!",
-            //% "Engine has crashed because a physical memory address error"
             "Engine has crashed because a physical memory address error");
         abortEngine(signal);
     }
@@ -661,10 +688,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
         pLogFatal("<Wrong CPU Instruction>\n"
                   STACK_FORMAT,
                   stack.c_str(), g_messageToUser);
-        XMsgBox::errorMsgBox(
-            //% "Wrong CPU Instruction!"
+        g_msgBoxHook(
             "Wrong CPU Instruction!",
-            //% "Engine has crashed because a wrong CPU instruction"
             "Engine has crashed because a wrong CPU instruction");
         abortEngine(signal);
     }
@@ -718,10 +743,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
                       stack.c_str(), g_messageToUser);
         }
 
-        XMsgBox::errorMsgBox(
-            //% "Wrong arithmetical operation"
+        g_msgBoxHook(
             "Wrong arithmetical operation",
-            //% "Engine has crashed because of a wrong arithmetical operation!"
             "Engine has crashed because of a wrong arithmetical operation!");
         abortEngine(signal);
     }
@@ -732,10 +755,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
         pLogFatal("<Aborted!>\n"
                   STACK_FORMAT,
                   stack.c_str(), g_messageToUser);
-        XMsgBox::errorMsgBox(
-            //% "Aborted"
+        g_msgBoxHook(
             "Aborted",
-            //% "Engine has been aborted because critical error was occouped."
             "Engine has been aborted because critical error was occouped.");
         abortEngine(signal);
     }
@@ -779,12 +800,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
                       stack.c_str(), g_messageToUser);
         }
 
-        XMsgBox::errorMsgBox(
-            //% "Segmentation fault"
+        g_msgBoxHook(
             "Segmentation fault",
-            /*% "Engine has crashed because of a Segmentation fault.\n"
-                "Run debugging with a built in debug mode application\n"
-                "and retry your recent actions to get more detailed information." */
             "Engine has crashed because of a Segmentation fault.\n"
             "Run debugging with a built in debug mode application\n"
             "and retry your recent actions to get more detailed information.");
@@ -794,10 +811,8 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
     case SIGINT:
     {
         pLogFatal("<Interrupted!>");
-        XMsgBox::errorMsgBox(
-            //% "Interrupt"
+        g_msgBoxHook(
             "Interrupt",
-            //% "Engine has been interrupted"
             "Engine has been interrupted");
         abortEngine(signal);
     }
@@ -806,10 +821,20 @@ static void handle_signal(int signal, siginfo_t *siginfo, void * /*context*/)
         return;
     }
 }
+#endif // defined(PGE_ENABLE_SIGNAL_HOOKS)
 
 #ifndef THEXTECH_NO_SDL_BUILD
 static SDL_AssertState custom_sdl_handler(const SDL_AssertData *data, void *userdata)
 {
+    CrashHandler::logAssertInfo(data);
+    return SDL_GetDefaultAssertionHandler()(data, userdata);
+}
+#endif
+
+void CrashHandler::logAssertInfo(const void* data_p)
+{
+    const SDL_AssertData *data = reinterpret_cast<const SDL_AssertData*>(data_p);
+
     std::string stack = getStacktrace();
     pLogFatal("<Assertion condition has failed>:\n"
               "---------------------------------------------------------\n"
@@ -824,13 +849,13 @@ static SDL_AssertState custom_sdl_handler(const SDL_AssertData *data, void *user
               stack.c_str(),
               g_messageToUser);
 
-    return SDL_GetDefaultAssertionHandler()(data, userdata);
+    // Finalize logger
+    CloseLog();
 }
-#endif
 
 #if defined(HAS_SIG_INFO)
 static struct sigaction act;
-#else
+#elif defined(PGE_ENABLE_SIGNAL_HOOKS)
 #   if _WIN32
 struct siginfo_t;
 #   endif
@@ -855,7 +880,7 @@ void CrashHandler::initSigs()
 
     std::set_new_handler(&crashByFlood);
     std::set_terminate(&crashByUnhandledException);
-#if defined(HAS_SIG_INFO) // Unsupported signals by Windows
+#if defined(HAS_SIG_INFO) && defined(PGE_ENABLE_SIGNAL_HOOKS) // Unsupported signals by Windows
     memset(&act, 0, sizeof(struct sigaction));
     sigemptyset(&act.sa_mask);
     act.sa_sigaction = handle_signal;
@@ -873,7 +898,7 @@ void CrashHandler::initSigs()
     sigaction(SIGSEGV, &act, nullptr);
     sigaction(SIGINT,  &act, nullptr);
     sigaction(SIGABRT, &act, nullptr);
-#else // HAS_SIG_INFO
+#elif defined(PGE_ENABLE_SIGNAL_HOOKS) // HAS_SIG_INFO
     signal(SIGILL,  &handle_signalWIN32);
     signal(SIGFPE,  &handle_signalWIN32);
     signal(SIGSEGV, &handle_signalWIN32);
@@ -888,4 +913,5 @@ void CrashHandler::initSigs()
 #   endif
 #endif // HAS_SIG_INFO
 }
+
 /* Signals End */

lib/CrashHandler/crash_handler.h
@@ -20,13 +20,18 @@
 #ifndef CRASHHANDLER_H
 #define CRASHHANDLER_H
 
+#include <string>
+
 class CrashHandler
 {
 public:
     CrashHandler() = default;
+    typedef void (*MsgBoxHook_t)(const std::string &title, const std::string &message);
+    static void setCrashMsgBoxHook(MsgBoxHook_t hook);
     static void crashByUnhandledException();
     static void crashByFlood();
     static void initSigs();
+    static void logAssertInfo(const void *data);
 };
 
 #endif // CRASHHANDLER_H

src/game_main.cpp
@@ -27,6 +27,11 @@
 #include <sysapp/launch.h>
 #endif
 
+#if defined(THEXTECH_ASSERTS_INGAME_MESSAGE) && !defined(THEXTECH_NO_SDL_BUILD)
+#   include <CrashHandler/crash_handler.h>
+#   include <signal.h>
+#endif
+
 #include <Logger/logger.h>
 #include <Utils/files.h>
 #include <AppPath/app_path.h>
@@ -301,6 +306,50 @@ void MainLoadAll()
 }
 
 
+#if defined(THEXTECH_ASSERTS_INGAME_MESSAGE) && !defined(THEXTECH_NO_SDL_BUILD)
+static SDL_AssertState ingame_assert_sdl_handler(const SDL_AssertData *data, void *)
+{
+    CrashHandler::logAssertInfo(data);
+    g_MessageType = MESSAGE_TYPE_SYS_FATAL_ASSERT;
+    MessageTitle = "Fatal error!";
+    MessageText =  fmt::sprintf_ne("Assertion condition has failed:\n"
+                                   "\n"
+                                   "File: %s(%d)\n"
+                                   "Function: %s\n"
+                                   "Condition: %s\n"
+                                   "\n"
+                                   "Game will be closed.\n\n"
+                                   "See log for details:\n"
+                                   "%s",
+                                   data->filename, data->linenum,
+                                   data->function,
+                                   data->condition,
+                                   getLogFilePath().c_str());
+    PauseGame(PauseCode::Message);
+
+    return SDL_ASSERTION_ABORT;
+}
+
+static void ingame_crash_msg_handler(const std::string &title, const std::string &message)
+{
+    CloseLog(); // Before this, last log message was written, so, no more logs will be printed
+    g_MessageType = MESSAGE_TYPE_SYS_FATAL_ASSERT;
+    MessageTitle = "Fatal error!";
+    signal(SIGABRT, SIG_DFL);
+    MessageText =  fmt::sprintf_ne("%s\n"
+                                  "\n"
+                                  "%s\n\n"
+                                  "Game will be closed.\n\n"
+                                  "See log for details:\n"
+                                  "%s",
+                                  title.c_str(),
+                                  message.c_str(),
+                                  getLogFilePath().c_str());
+    PauseGame(PauseCode::Message);
+}
+#endif
+
+
 int GameMain(const CmdLineSetup_t &setup)
 {
     Player_t blankPlayer;
@@ -403,6 +452,11 @@ int GameMain(const CmdLineSetup_t &setup)
     gfxLoaderThreadingMode = true;
 #endif
     XWindow::show(); // Don't show window until playing an initial sound
+
+#if defined(THEXTECH_ASSERTS_INGAME_MESSAGE) && !defined(THEXTECH_NO_SDL_BUILD)
+    SDL_SetAssertionHandler(&ingame_assert_sdl_handler, NULL);
+    CrashHandler::setCrashMsgBoxHook(&ingame_crash_msg_handler);
+#endif
 
     if(!setup.testLevelMode && !init_failure)
         PlayInitSound();

src/globals.h
@@ -1323,6 +1323,8 @@ enum MessageType
     MESSAGE_TYPE_SYS_WARNING,
     // Has title, red colour
     MESSAGE_TYPE_SYS_ERROR,
+    // Has title, red colour, game will quit after closing this
+    MESSAGE_TYPE_SYS_FATAL_ASSERT,
 };
 extern MessageType g_MessageType;
 

src/graphics.h
@@ -142,6 +142,11 @@ void DrawMessage(const std::string& SuperText);
  */
 void DrawMessage(const UTF8CharMap_t& SuperTextMap);
 
+/*!
+ * \brief This is a very special function that runs only last state of the texture
+ */
+void UpdateGraphicsFatalAssert();
+
 // Public Sub SetRes()
 // void SetRes(); //deprecated
 // Public Function CheckKey(newStrizzle As String) As String

src/graphics/gfx_message.cpp
@@ -4,6 +4,9 @@
 #include "../graphics.h"
 #include "core/render.h"
 #include "../gfx.h"
+#include "../draw_planes.h"
+#include "../frame_timer.h"
+#include "../config.h"
 
 #include "fontman/font_manager_private.h"
 #include "fontman/font_manager.h"
@@ -107,6 +110,7 @@ void DrawMessage(const UTF8CharMap_t &SuperTextMap)
         titleColour = {0xf5, 0xff, 0x1a};
         break;
     case MESSAGE_TYPE_SYS_ERROR:
+    case MESSAGE_TYPE_SYS_FATAL_ASSERT:
         messageColour = {0x61, 0x00, 0x0F};
         titleColour = {0xf5, 0xff, 0x1a};
         break;
@@ -246,4 +250,26 @@ void DrawMessage(const UTF8CharMap_t &SuperTextMap)
         BoxY += lineHeight;
         firstLine = false;
     }
+}
+
+void UpdateGraphicsFatalAssert()
+{
+    cycleNextInc();
+
+    if(g_config.enable_frameskip && !TakeScreen && frameSkipNeeded())
+        return;
+
+    XRender::setTargetTexture();
+    XRender::resetViewport();
+    XRender::setDrawPlane(PLANE_LVL_META);
+
+    if(PrintFPS > 0 && g_config.show_fps)
+        SuperPrint(std::to_string(PrintFPS), 1, XRender::TargetOverscanX + 8, 8, {0, 255, 0});
+
+    if(MessageTextMap.empty())
+        DrawMessage(MessageText);
+    else
+        DrawMessage(MessageTextMap);
+
+    XRender::repaint();
 }

commit b5de6f3b69d17264b26e1ee9f653ab7ad930c857

sound_msgsnd.cpp: fix possible crash in error box
(Introduced in ffea940b)

src/sound.cpp
@@ -105,7 +105,7 @@ const AudioDefaults_t g_audioDefaults =
 #endif
 
 static Mix_Music *g_curMusic = nullptr;
-static bool g_mixerLoaded = false;
+bool g_mixerLoaded = false;
 
 //! most recent argument to StartMusic. Could be a world map music ID or a section index.
 static constexpr int s_null_music = -5;

src/sound/sound_msgsnd.cpp
@@ -23,6 +23,8 @@
 #include "../sound.h"
 #include "sound_msgsnd.h"
 
+extern bool g_mixerLoaded;
+
 static unsigned char s_sndGlas[] =
 {
     0x4d, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06, 0x00, 0x01, 0x00, 0x05,
@@ -97,6 +99,9 @@ static unsigned char s_sndMsg[] =
 #ifndef CUSTOM_AUDIO
 void playFallbackSfx(int sfxId, int loops, int volume)
 {
+    if(!g_mixerLoaded)
+        return;
+
     SDL_RWops *sfx = nullptr;
 
     switch(sfxId)