cb85c911ca3b22fb9961fccfddfa8980b15c4719
Wohlstand <admin@wohlnet.ru>
2024-12-13 04:00:01 +0000

    CMake: Added package building code for Haiku
    The "thextech_bin" package will be built

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -1,8 +1,13 @@
 # adds a custom target for git version (always built)
 add_custom_target(git_version ALL)
 
+# Report about improperly use of PRE_BUILD, PRE_LINK, and POST_BUILD keywords
+if(POLICY CMP0175)
+    cmake_policy(SET CMP0175 NEW)
+endif()
+
 # creates git_version.h using cmake script
-add_custom_command(TARGET git_version
+add_custom_command(TARGET git_version PRE_BUILD
     COMMAND ${CMAKE_COMMAND}
         -DSOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR} -DOVERRIDE_GIT_BRANCH=${OVERRIDE_GIT_BRANCH}
         -DFLATPAK_BUILD=${FLATPAK_BUILD}
