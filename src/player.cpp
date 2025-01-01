TheXTech/TheXTech

commit b4052aebe290bc7631ad89de49454c90d1b72f56

Player_t: split RespawnY from Effect2
(Keep using double for RespawnY, use vbint_t for Effect2)

src/player.cpp
@@ -494,7 +494,7 @@ void DodgePlayers(int plr_A)
             pLoc.Y = old_Y;
 
             Player[plr_A].Effect = PLREFF_RESPAWN;
-            Player[plr_A].Effect2 = pLoc.Y;
+            Player[plr_A].RespawnY = pLoc.Y;
             break;
         }
     }
@@ -628,6 +628,7 @@ void SetupPlayers()
         // moved from below to here
         Player[A].Effect = PLREFF_NORMAL;
         Player[A].Effect2 = 0;
+        Player[A].RespawnY = 0;
 
         // modern multiplayer placement code
         if(g_config.multiplayer_pause_controls)
@@ -4172,7 +4173,7 @@ void RespawnPlayer(int A, double Direction, double CenterX, double StopY, const 
     Player[A].Location.SpeedY = 0;
     Player[A].Effect = PLREFF_RESPAWN;
     // location where player stops flashing
-    Player[A].Effect2 = StopY - Player[A].Location.Height;
+    Player[A].RespawnY = StopY - Player[A].Location.Height;
     Player[A].Location.Y = -target_screen.Y - Player[A].Location.Height;
     Player[A].Location.X = CenterX - Player[A].Location.Width / 2.0;
 }
@@ -4184,7 +4185,7 @@ void RespawnPlayerTo(int A, int TargetPlayer)
     // don't lose a player when it targets a player who is already respawning
     double StopY;
     if(Player[TargetPlayer].Effect == PLREFF_RESPAWN)
-        StopY = Player[TargetPlayer].Effect2 + Player[TargetPlayer].Location.Height;
+        StopY = Player[TargetPlayer].RespawnY + Player[TargetPlayer].Location.Height;
     else if(Player[TargetPlayer].Mount == 2)
         StopY = Player[TargetPlayer].Location.Y;
     else
@@ -4210,6 +4211,7 @@ void RespawnPlayerTo(int A, int TargetPlayer)
             Player[A].Dead = true;
             Player[A].Effect = PLREFF_COOP_WINGS;
             Player[A].Effect2 = 0;
+            Player[A].RespawnY = 0;
         }
         // respawn them to the target player's warp otherwise
         else
@@ -4221,11 +4223,11 @@ void RespawnPlayerTo(int A, int TargetPlayer)
             Player[A].Location.X = warp_exit.X + warp_exit.Width / 2 - Player[A].Location.Width / 2;
 
             if(Player[TargetPlayer].Effect == PLREFF_WARP_PIPE && warp_exit_dir == 1)
-                Player[A].Effect2 = warp_exit.Y;
+                Player[A].RespawnY = warp_exit.Y;
             else
-                Player[A].Effect2 = warp_exit.Y + warp_exit.Height - Player[A].Location.Height;
+                Player[A].RespawnY = warp_exit.Y + warp_exit.Height - Player[A].Location.Height;
 
-            Player[A].Location.Y = Player[A].Effect2 - target_vscreen.Height * 3 / 4;
+            Player[A].Location.Y = Player[A].RespawnY - target_vscreen.Height * 3 / 4;
         }
     }
 }
@@ -6025,7 +6027,7 @@ void PlayerEffects(const int A)
 
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2, 14))
+        if(p.Effect2 == 14)
         {
             p.Immune += 50;
             p.Immune2 = true;
@@ -6253,7 +6255,7 @@ void PlayerEffects(const int A)
 
         for(B = 1; B <= numPlayers; B++)
         {
-            if(B != A && (Player[B].Effect == PLREFF_NORMAL || fEqual(B, p.Effect2)) && !Player[B].Dead && Player[B].TimeToLive == 0 && CheckCollision(p.Location, Player[B].Location))
+            if(B != A && (Player[B].Effect == PLREFF_NORMAL || B == p.Effect2) && !Player[B].Dead && Player[B].TimeToLive == 0 && CheckCollision(p.Location, Player[B].Location))
                 tempBool = false;
         }
 
@@ -6319,9 +6321,9 @@ void PlayerEffects(const int A)
 
         p.Location.Y += 2.2;
 
-        if(p.Location.Y >= p.Effect2)
+        if(p.Location.Y >= p.RespawnY)
         {
-            p.Location.Y = p.Effect2;
+            p.Location.Y = p.RespawnY;
             tempBool = true;
             for(B = 1; B <= numPlayers; B++)
             {
@@ -6332,7 +6334,7 @@ void PlayerEffects(const int A)
             if(tempBool)
             {
                 p.Effect = PLREFF_NORMAL;
-                p.Effect2 = 0;
+                p.RespawnY = 0;
                 p.Immune = 50;
                 p.Immune2 = false;
                 p.Location.SpeedY = 0.01;
@@ -6408,7 +6410,7 @@ void PlayersEnsureNearby(const Screen_t& screen)
         const Location_t& pLoc = p.Location;
 
         double p_x = pLoc.X + pLoc.Width / 2;
-        double p_top = (p.Effect == PLREFF_RESPAWN) ? p.Effect2 : pLoc.Y;
+        double p_top = (p.Effect == PLREFF_RESPAWN) ? p.RespawnY : pLoc.Y;
         double p_y = p_top + pLoc.Height / 2;
 
         // dead players don't affect camera
@@ -6472,7 +6474,7 @@ void PlayersEnsureNearby(const Screen_t& screen)
 
         if(!p.Dead && p.TimeToLive == 0)
         {
-            double p_top = (p.Effect == PLREFF_RESPAWN) ? p.Effect2 : pLoc.Y;
+            double p_top = (p.Effect == PLREFF_RESPAWN) ? p.RespawnY : pLoc.Y;
             double dist = (pLoc.X - cx) * (pLoc.X - cx) + (p_top - cy) * (p_top - cy);
 
             if(closest == 0 || closest_dist > dist)
@@ -6492,7 +6494,7 @@ void PlayersEnsureNearby(const Screen_t& screen)
 
     // if the winner is currently respawning, place them at their target loc
     if(pClosest.Effect == PLREFF_RESPAWN)
-        pClosest.Location.Y = pClosest.Effect2;
+        pClosest.Location.Y = pClosest.RespawnY;
 
     // move all players to winning player's location, and set effect if alive
     for(int plr_i = 0; plr_i < screen.player_count; plr_i++)
@@ -6526,6 +6528,8 @@ void PlayersEnsureNearby(const Screen_t& screen)
 
         if(p.Dead || p.TimeToLive != 0)
             continue;
+
+        p.RespawnY = 0;
 
         if(in_door_scroll)
         {
@@ -6779,7 +6783,7 @@ void SwapCharacter(int A, int Character, bool FromBlock)
     {
         double saved_respawn_StopY = 0;
         if(p.Effect == PLREFF_RESPAWN)
-            saved_respawn_StopY = p.Effect2 + p.Location.Height;
+            saved_respawn_StopY = p.RespawnY + p.Location.Height;
 
         // make player bottom match old player bottom, to avoid floor glitches
         UnDuck(Player[A]);
@@ -6787,7 +6791,7 @@ void SwapCharacter(int A, int Character, bool FromBlock)
 
         // if player effect is 6 (respawn downwards), update target similarly
         if(p.Effect == PLREFF_RESPAWN)
-            p.Effect2 = saved_respawn_StopY - p.Location.Height;
+            p.RespawnY = saved_respawn_StopY - p.Location.Height;
     }
 
     if(!LevelSelect)

src/globals.h
@@ -693,8 +693,10 @@ struct Player_t
     bool CanAltJump = false;
 //    Effect As Integer 'for various effects like shrinking/growing/warping
     PlayerEffect Effect = PLREFF_NORMAL;
-//    Effect2 As Double 'counter for the effects
-    double Effect2 = 0.0;
+//    Effect2 As Double 'counter for the effects (was double)
+    vbint_t Effect2 = 0;
+//    NEW: this previously used the same storage as Effect2
+    double RespawnY = 0.0;
 //    Duck As Boolean 'true if ducking
     bool Duck = false;
 //    DropRelease As Boolean

src/graphics.cpp
@@ -51,7 +51,7 @@ void GetvScreen(vScreen_t& vscreen)
         pLoc.Height = 0;
 
     // this check is new because players can now respawn in 1P mode through DropAdd
-    double pLocY = (p.Effect == PLREFF_RESPAWN) ? p.Effect2 : pLoc.Y;
+    double pLocY = (p.Effect == PLREFF_RESPAWN) ? p.RespawnY : pLoc.Y;
 
     vscreen.X = -pLoc.X + (vscreen.Width * 0.5) - pLoc.Width / 2.0;
     vscreen.Y = -pLocY + (vscreen.Height * 0.5) - vScreenYOffset - pLoc.Height;
@@ -112,7 +112,7 @@ void GetvScreenAverage(vScreen_t& vscreen)
         {
             vscreen.X += -Player[A].Location.X - Player[A].Location.Width / 2.0;
 
-            double pLocY = (Player[A].Effect == PLREFF_RESPAWN) ? Player[A].Effect2 : Player[A].Location.Y;
+            double pLocY = (Player[A].Effect == PLREFF_RESPAWN) ? Player[A].RespawnY : Player[A].Location.Y;
 
             if(Player[A].Mount == 2)
                 vscreen.Y += -pLocY;
@@ -256,7 +256,7 @@ void GetvScreenAverage3(vScreen_t& vscreen)
         double pr = pl + plr.Location.Width;
 
         // DANGER: consider case where players are free-fall and a player respawns
-        double pLocY = (plr.Effect == PLREFF_RESPAWN) ? plr.Effect2 : plr.Location.Y;
+        double pLocY = (plr.Effect == PLREFF_RESPAWN) ? plr.RespawnY : plr.Location.Y;
 
         double by = pLocY + plr.Location.Height;
 
@@ -587,7 +587,7 @@ void PlayerWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
     // .Effect2 => 100  -- Delay at next spot
     // .Effect2 = 2     -- Exiting
     // .Effect2 = 3     -- Done
-    if(player.Effect2 == 0.0)
+    if(player.Effect2 == 0)
     {
         if(warp_dir_enter == 3) // Moving down
         {
@@ -614,7 +614,7 @@ void PlayerWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
                 tempLocation.X = warp_enter.X;
         }
     }
-    else if(Maths::iRound(player.Effect2) == 2)
+    else if(player.Effect2 == 2)
     {
         if(warp_dir_exit == 3) // Moving up
         {
@@ -642,7 +642,7 @@ void PlayerWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
         }
     }
 
-    if(Maths::iRound(player.Effect2) == 1 || player.Effect2 >= 100)
+    if(player.Effect2 == 1 || player.Effect2 >= 100)
         tempLocation.Height = 0;
 
     if(tempLocation.Height < 0)
@@ -673,7 +673,7 @@ void NPCWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
     // player(a).effect2 => 100  -- Delay at next spot
     // player(a).effect2 = 2     -- Exiting
     // player(a).effect2 = 3     -- Done
-    if(player.Effect2 == 0.0)
+    if(player.Effect2 == 0)
     {
         if(warp_dir_enter == 3) // Moving down
         {
@@ -700,7 +700,7 @@ void NPCWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
                 tempLocation.X = warp_enter.X;
         }
     }
-    else if(Maths::iRound(player.Effect2) == 2)
+    else if(player.Effect2 == 2)
     {
         if(warp_dir_exit == 3) // Moving up
         {
@@ -728,7 +728,7 @@ void NPCWarpGFX(int A, IntegerLocation_t &tempLocation, int &X2, int &Y2)
         }
     }
 
-    if(Maths::iRound(player.Effect2) == 1 || player.Effect2 >= 100)
+    if(player.Effect2 == 1 || player.Effect2 >= 100)
         tempLocation.Height = 0;
 
     if(tempLocation.Height < 0)

src/graphics/gfx_screen.cpp
@@ -233,8 +233,8 @@ void DynamicScreen(Screen_t& screen, bool mute)
     Player_t& p1 = Player[screen.players[0]];
     Player_t& p2 = Player[screen.players[1]];
 
-    double p1LocY = (p1.Effect == PLREFF_RESPAWN) ? p1.Effect2 : p1.Location.Y;
-    double p2LocY = (p2.Effect == PLREFF_RESPAWN) ? p2.Effect2 : p2.Location.Y;
+    double p1LocY = (p1.Effect == PLREFF_RESPAWN) ? p1.RespawnY : p1.Location.Y;
+    double p2LocY = (p2.Effect == PLREFF_RESPAWN) ? p2.RespawnY : p2.Location.Y;
 
     if(!p1.Dead && !p2.Dead)
     {

src/player/player_screen_logic.cpp
@@ -415,7 +415,7 @@ void PlayerLevelWrapLogic(int A)
             {
                 RemoveFromPet(o_A);
                 o_p.Effect = PLREFF_RESPAWN;
-                o_p.Effect2 = o_p.Location.Y;
+                o_p.RespawnY = o_p.Location.Y;
             }
         }
 

src/player/player_warp_logic.cpp
@@ -485,7 +485,7 @@ void PlayerEffectWarpPipe(int A)
         // trigger fader when there are 16 pixels left (for normal effects) or 0 pixels left (for none or scroll)
         s_WarpFaderLogic(false, A, warp.transitEffect, warp_enter, Maths::iRound(leftToGoal) == 16, !is_level_quit && !same_section && Maths::iRound(leftToGoal) == 0);
     }
-    else if(fEqual(p.Effect2, 1))  // Exiting pipe (initialization)
+    else if(p.Effect2 == 1)  // Exiting pipe (initialization)
     {
         if(warp.NoYoshi)
         {
@@ -726,7 +726,7 @@ void PlayerEffectWarpPipe(int A)
                 PlaySoundSpatial(SFX_Warp, p.Location);
         }
     }
-    else if(fEqual(p.Effect2, 2)) // Proceeding the pipe exiting
+    else if(p.Effect2 == 2) // Proceeding the pipe exiting
     {
         if(!backward && warp.cannonExit)
         {
@@ -885,7 +885,7 @@ void PlayerEffectWarpPipe(int A)
         if(p.HoldingNPC > 0 && p.HoldingNPC <= numNPCs)
             treeNPCUpdate(p.HoldingNPC);
     }
-    else if(fEqual(p.Effect2, 3)) // Finishing the pipe exiting / shooting
+    else if(p.Effect2 == 3) // Finishing the pipe exiting / shooting
     {
         if(!backward && warp.cannonExit)
         {
@@ -1010,12 +1010,12 @@ void PlayerEffectWarpDoor(int A)
         p.Frame = 1;
 
     // trigger warp fader when p.Effect2 is 5 (for normal animations) or 20 (for none/scroll animations)
-    s_WarpFaderLogic(false, A, warp.transitEffect, warp_enter, fEqual(p.Effect2, 5), !is_level_quit && !same_section && fEqual(p.Effect2, 20));
+    s_WarpFaderLogic(false, A, warp.transitEffect, warp_enter, p.Effect2 == 5, !is_level_quit && !same_section && (p.Effect2 == 20));
 
     // teleport other players into door in shared screen mode
     Screen_t& screen = ScreenByPlayer(A);
     bool is_shared_screen = (screen.Type == 3);
-    if(is_shared_screen && fEqual(p.Effect2, 15))
+    if(is_shared_screen && (p.Effect2 == 15))
     {
         int vscreen_A = vScreenIdxByPlayer(A);
         bool do_tele = is_shared_screen && !vScreenCollision(vscreen_A, warp_exit);
@@ -1060,7 +1060,7 @@ void PlayerEffectWarpDoor(int A)
     }
 
     // start the scroll effect
-    if(do_scroll && fEqual(p.Effect2, 29))
+    if(do_scroll && p.Effect2 == 29)
     {
         s_InitWarpScroll(p, warp_enter, warp_exit, 30);
         SoundPause[SFX_Door] = 60;
@@ -1200,7 +1200,7 @@ void PlayerEffectWarpWait(int A)
     Player_t& p = Player[A];
 
     // door exit holding pattern (exit blocked)
-    if(fEqual(p.Effect2, 131))
+    if(p.Effect2 == 131)
     {
         bool tempBool = false;
         for(int B = 1; B <= numPlayers; B++)
@@ -1227,7 +1227,7 @@ void PlayerEffectWarpWait(int A)
     else if(p.Effect2 <= 130)
     {
         p.Effect2 -= 1;
-        if(fEqual(p.Effect2, 100))
+        if(p.Effect2 == 100)
         {
             Screen_t& screen = ScreenByPlayer(A);
 
@@ -1243,7 +1243,7 @@ void PlayerEffectWarpWait(int A)
     else if(p.Effect2 <= 300)
     {
         p.Effect2 -= 1;
-        if(fEqual(p.Effect2, 200))
+        if(p.Effect2 == 200)
         {
             Screen_t& screen = ScreenByPlayer(A);
 
@@ -1258,7 +1258,7 @@ void PlayerEffectWarpWait(int A)
     else if(p.Effect2 <= 1000) // Start Wait for pipe
     {
         p.Effect2 -= 1;
-        if(fEqual(p.Effect2, 900))
+        if(p.Effect2 == 900)
         {
             p.Effect = PLREFF_WARP_PIPE;
             p.Effect2 = 100;
@@ -1283,7 +1283,7 @@ void PlayerEffectWarpWait(int A)
     {
         p.Effect2 -= 1;
 
-        if(fEqual(p.Effect2, 1900))
+        if(p.Effect2 == 1900)
         {
             s_TriggerDoorEffects(static_cast<Location_t>(Warp[p.Warp].Exit), false);
 
@@ -1302,7 +1302,7 @@ void PlayerEffectWarpWait(int A)
                 PlaySoundSpatial(SFX_Door, p.Location);
         }
         // new code to replicate SMBX 1.3 logic setting Effect2 to 30
-        else if(fEqual(p.Effect2, 1870))
+        else if(p.Effect2 == 1870)
         {
             // Trigger an exit event at door warp that was used to enter the level
             if(Warp[p.Warp].eventExit != EVENT_NONE)
@@ -1318,10 +1318,10 @@ void PlayerEffectWarpWait(int A)
 
         auto &w = Warp[p.Warp];
 
-        if(g_config.EnableInterLevelFade && (w.MapWarp || w.level != STRINGINDEX_NONE) && Maths::iRound(p.Effect2) == 2955 && !g_levelScreenFader.isFadingIn())
+        if(g_config.EnableInterLevelFade && (w.MapWarp || w.level != STRINGINDEX_NONE) && (p.Effect2 == 2955) && !g_levelScreenFader.isFadingIn())
             g_levelScreenFader.setupFader(2, 0, 65, ScreenFader::S_FADE);
 
-        if(fEqual(p.Effect2, 2920))
+        if(p.Effect2 == 2920)
         {
             if(w.MapWarp)
             {

commit eff468e08d5e3740e03cb83fbe5a5f9e42e9acc4

player.cpp: ouch, fix player grow/shrink effects
(Fixes bug introduced in b4052aebe290bc7631ad89de49454c90d1b72f56)

src/player.cpp
@@ -5580,7 +5580,7 @@ void PlayerEffects(const int A)
     {
         p.Frame = 1;
         p.Effect2 += 1;
-        if(p.Effect2 / 5 == static_cast<int>(floor(static_cast<double>(p.Effect2 / 5))))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 1)
             {
@@ -5643,7 +5643,7 @@ void PlayerEffects(const int A)
         }
         p.Frame = 1;
         p.Effect2 += 1;
-        if(p.Effect2 / 5 == static_cast<int>(floor(static_cast<double>(p.Effect2 / 5))))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 1)
             {
@@ -5715,7 +5715,7 @@ void PlayerEffects(const int A)
         p.Frame = 1;
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2 / 5, std::floor(p.Effect2 / 5)))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 2)
                 p.State = prev_state;
@@ -5747,7 +5747,7 @@ void PlayerEffects(const int A)
         }
         p.Frame = 1;
         p.Effect2 += 1;
-        if(p.Effect2 / 5 == static_cast<int>(floor(static_cast<double>(p.Effect2 / 5))))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 2)
                 p.State = 3;
@@ -5778,7 +5778,7 @@ void PlayerEffects(const int A)
         p.Frame = 1;
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2 / 5, std::floor(p.Effect2 / 5)))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 2)
                 p.State = 7;
@@ -5842,7 +5842,7 @@ void PlayerEffects(const int A)
 
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2 / 5, std::floor(p.Effect2 / 5.0)))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 1 && p.Character != 5)
             {
@@ -5894,7 +5894,7 @@ void PlayerEffects(const int A)
 
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2 / 5, std::floor(p.Effect2 / 5.0)))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 1 && p.Character != 5)
             {
@@ -5944,7 +5944,7 @@ void PlayerEffects(const int A)
 
         p.Effect2 += 1;
 
-        if(fEqual(p.Effect2 / 5, std::floor(p.Effect2 / 5.0)))
+        if(p.Effect2 % 5 == 0)
         {
             if(p.State == 1 && p.Character != 5)
             {