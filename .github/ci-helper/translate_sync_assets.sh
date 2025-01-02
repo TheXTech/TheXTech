7ae8bb3db67263b38e3ccc2fb53cadf669cf743c
Wohlstand <admin@wohlnet.ru>
2025-01-02 05:05:57 +0300

    CI: Allow update languages at different branches
    As Yingchun requests, do langs sync of Nostalgic Paradise at the `development` branch only.

--- .github/ci-helper/translate_sync_assets.sh
+++ .github/ci-helper/translate_sync_assets.sh
@@ -6,6 +6,35 @@ WORKDIR="$PWD/.github/ci-helper"
 
 mkdir -p build-git
 
+function update_repo()
+{
+    $q=$1
+    $branch=$2
+
+    if [[ "$branch" != "" ]]; then
+        $branch_command=-b $branch
+    fi
+
+    echo "--------------- $q ---------------"
+    cd "${GIT_ROOT}"
+    git clone "$q" --depth 1 $branch_command repo
+
+    cd "${WORKDIR}"
+    bash translate_update.sh "${GIT_ROOT}/repo"
+
+    cd "${GIT_ROOT}/repo"
+    if [[ ! -z $(git status -s) ]]; then
+        echo "-- Found updated languages, commiting..."
+        git add --all
+        git commit --author="${GIT_AUTHOR}" -m "Synchronized translations"
+        git push
+    else
+        echo "-- No updates found, skipping..."
+    fi
+    cd ..
+    rm -Rf repo
+}
+
 # FIXME: Replace this hardcoded list with a server-side one to don't re-commit this list
 # every time
 for q in \
@@ -23,24 +52,10 @@ for q in \
     "git@gitea.wohlsoft.ru:Games-for-TheXTech/thextech-super-talking-time-bros-2-5-v1-2.git" \
     ;
 do
-    echo "--------------- $q ---------------"
-    cd "${GIT_ROOT}"
-    git clone "$q" --depth 1 repo
-
-    cd "${WORKDIR}"
-    bash translate_update.sh "${GIT_ROOT}/repo"
-
-    cd "${GIT_ROOT}/repo"
-    if [[ ! -z $(git status -s) ]]; then
-        echo "-- Found updated languages, commiting..."
-        git add --all
-        git commit --author="${GIT_AUTHOR}" -m "Synchronized translations"
-        git push
-    else
-        echo "-- No updates found, skipping..."
-    fi
-    cd ..
-    rm -Rf repo
+    update_repo "$q"
 done
+
+# Nostalgic Paradise needs another branch
+update_repo git@gitea.wohlsoft.ru:Games-for-TheXTech/nostalgic-paradise.git development
 
 cd "${OLD}"
