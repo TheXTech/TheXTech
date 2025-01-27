d961a486569420b452fdd5051309f443318c1685
Wohlstand <admin@wohlnet.ru>
2025-01-27 21:33:14 +0300

    Android: Fixed the git version inconsistency for F-Droid

--- cmake/git_version_update.cmake
+++ cmake/git_version_update.cmake
@@ -35,7 +35,7 @@ execute_process(
 )
 
 # flatpak-builder breaks any commands that check the git tree
-if(NOT "${GIT_DIRTY_STRING}" STREQUAL "" AND NOT FLATPAK_BUILD)
+if(NOT "${GIT_DIRTY_STRING}" STREQUAL "" AND NOT FLATPAK_BUILD AND NOT FDROID_BUILD)
     # on CI, display the git diff in the log
     if(OVERRIDE_GIT_BRANCH OR DEFINED ENV{CI})
         message("=== git index dirty, see diff:")
