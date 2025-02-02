TheXTech/TheXTech

commit 9fd0f8559d93b195fd1bf0908691bb2dd08d43ad

Maintain basic NPC queues

src/layers.cpp
@@ -38,6 +38,7 @@
 #include "main/trees.h"
 #include "main/block_table.h"
 
+#include "npc/npc_queues.h"
 int numLayers = 0;
 RangeArr<Layer_t, 0, maxLayers> Layer;
 
@@ -397,6 +398,8 @@ void ShowLayer(layerindex_t L, bool NoEffect)
             {
                 NPC[A].Active = true;
                 NPC[A].TimeLeft = 1;
+
+                NPCQueues::Active.insert(A);
             }
             CheckSectionNPC(A);
     }
@@ -1420,6 +1423,8 @@ void syncLayersTrees_Block_SetHidden(int block) // set block hidden based on lay
 
 void syncLayers_AllNPCs()
 {
+    NPCQueues::clear();
+
     for(int npc = 1; npc <= numNPCs; npc++)
     {
         syncLayers_NPC(npc);
@@ -1435,6 +1440,8 @@ void syncLayers_NPC(int npc)
         else
             Layer[layer].NPCs.erase(npc);
     }
+
+    NPCQueues::update(npc);
 }
 
 void syncLayers_AllBGOs()

CMakeLists.txt
@@ -496,6 +496,7 @@ set(THEXTECH_SRC
     src/npc/npc_update.cpp
     src/npc/npc_frames.cpp
     src/npc/npc_bonus.cpp
+    src/npc/npc_queues.cpp
     src/player/player_update.cpp
 )
 
@@ -626,6 +627,8 @@ file(GLOB THEXTECH_HEADS_SRC
     src/main/*.h
     src/main/*.hpp
     src/main/QuadTree/*.h
+    src/npc/*.h
+    src/npc/*.hpp
     src/sound/fx/*.h
     src/sound/fx/*.hpp
 )

src/blocks.cpp
@@ -38,6 +38,7 @@
 #include "compat.h"
 #include "editor.h"
 
+#include "npc/npc_queues.h"
 #include "main/trees.h"
 
 void BlockHit(int A, bool HitDown, int whatPlayer)
@@ -2098,7 +2099,9 @@ void PSwitch(bool enabled)
                     nb.NPC = NPC[A].Type;
                     syncLayersTrees_Block(numBlock);
                 }
+
                 NPC[A].Killed = 9;
+                NPCQueues::Killed.push_back(A);
             }
         }
 
@@ -2237,6 +2240,7 @@ void PSwitch(bool enabled)
                     syncLayersTrees_Block(numBlock);
                 }
                 NPC[A].Killed = 9;
+                NPCQueues::Killed.push_back(A);
             }
         }
 
@@ -2424,7 +2428,7 @@ void PowBlock()
         }
     }
 
-    for(A = 1; A <= numNPCs; A++)
+    for(int A : NPCQueues::Active.no_change)
     {
         if(NPC[A].Active)
         {

src/editor/editor.cpp
@@ -1510,7 +1510,6 @@ void UpdateEditor()
                             NPC[numNPCs].Text = STRINGINDEX_NONE;
                             SetS(NPC[numNPCs].Text, GetS(EditorCursor.NPC.Text));
                         }
-                        syncLayers_NPC(numNPCs);
 //                        Netplay::sendData Netplay::AddNPC(numNPCs);
                         if(!MagicHand)
                         {
@@ -1530,6 +1529,7 @@ void UpdateEditor()
                             n.DefaultSpecial = int(n.Special);
                             CheckSectionNPC(numNPCs);
                         }
+                        syncLayers_NPC(numNPCs);
                     }
                 }
             }

src/graphics/gfx_update.cpp
@@ -48,6 +48,8 @@
 #include <fmt_format_ne.h>
 #include <Utils/maths.h>
 
+#include "npc/npc_queues.h"
+
 struct ScreenShake_t
 {
     double forceX = 0;
@@ -290,6 +292,9 @@ void UpdateGraphics(bool skipRepaint)
     int Z = 0;
     int numScreens = 0;
 
+    // just until NPC draw logic updated
+    NPCQueues::NoReset.clear();
+
     if(!GameIsActive)
         return;
 
@@ -362,6 +367,7 @@ void UpdateGraphics(bool skipRepaint)
 //                            if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                timeStr += "2b" + std::to_string(A) + LB;
                             NPC[A].Active = true;
+                            NPCQueues::Active.insert(A);
                         }
                         NPC[A].Reset[1] = false;
                         NPC[A].Reset[2] = false;
@@ -1066,6 +1072,7 @@ void UpdateGraphics(bool skipRepaint)
 //                            if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                timeStr += "2b" + std::to_string(A) + LB;
                             NPC[A].Active = true;
+                            NPCQueues::Active.insert(A);
                         }
                         NPC[A].Reset[1] = false;
                         NPC[A].Reset[2] = false;
@@ -1443,6 +1450,7 @@ void UpdateGraphics(bool skipRepaint)
 //                                if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                    timeStr += "2b" + std::to_string(A) + LB;
                             NPC[A].Active = true;
+                            NPCQueues::Active.insert(A);
                         }
 
                         NPC[A].Reset[1] = false;
@@ -1494,6 +1502,7 @@ void UpdateGraphics(bool skipRepaint)
 //                        if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                            timeStr += "2b" + std::to_string(A) + LB;
                         NPC[A].Active = true;
+                        NPCQueues::Active.insert(A);
                     }
                     NPC[A].Reset[1] = false;
                     NPC[A].Reset[2] = false;
@@ -1698,6 +1707,7 @@ void UpdateGraphics(bool skipRepaint)
 //                                if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                    timeStr += "2b" + std::to_string(A) + LB;
                                 NPC[A].Active = true;
+                                NPCQueues::Active.insert(A);
                             }
                             NPC[A].Reset[1] = false;
                             NPC[A].Reset[2] = false;
@@ -1930,6 +1940,7 @@ void UpdateGraphics(bool skipRepaint)
 //                                if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                    timeStr += "2b" + std::to_string(A) + LB;
                                 NPC[A].Active = true;
+                                NPCQueues::Active.insert(A);
                             }
                             NPC[A].Reset[1] = false;
                             NPC[A].Reset[2] = false;
@@ -2136,6 +2147,7 @@ void UpdateGraphics(bool skipRepaint)
 //                                    if(nPlay.Online == true && nPlay.NPCWaitCount >= 10 && nPlay.Mode == 0)
 //                                        timeStr += "2b" + std::to_string(A) + LB;
                                     NPC[A].Active = true;
+                                    NPCQueues::Active.insert(A);
                                 }
 
                                 NPC[A].Reset[1] = false;

src/main/cheat_code.cpp
@@ -46,6 +46,7 @@
 #include "game_info.h"
 #include "screen_quickreconnect.h"
 
+#include "npc/npc_queues.h"
 #include "cheat_code.h"
 
 
@@ -1155,6 +1156,7 @@ static void warioTime()
                 NPC[B].Location.X += -NPC[B].Location.Width / 2.0;
                 NPC[B].Location.SpeedX = 0;
                 NPC[B].Location.SpeedY = 0;
+                NPCQueues::Unchecked.push_back(B);
             }
         }
     }

src/main/level_file.cpp
@@ -660,7 +660,6 @@ bool OpenLevelData(LevelData &lvl, const std::string FilePath)
             npc.Reset[2] = true;
         }
 
-        syncLayers_NPC(numNPCs);
         CheckSectionNPC(numNPCs);
 
         if(npc.Type == NPCID_CHECKPOINT) // Is a checkpoint
@@ -689,6 +688,8 @@ bool OpenLevelData(LevelData &lvl, const std::string FilePath)
                     npc.Killed = 9;
             }
         }
+
+        syncLayers_NPC(numNPCs);
     }
 
 

commit aa88d8ea9fed13b896e7de01945b4a4ef2c358d6

Fix v1.3.6.1 bug where plant on hidden moving layer would not appear
Introduced in 9fd0f8559d93b195fd1bf0908691bb2dd08d43ad
Merged in 025515bb240ea0a9197149f6e4f44ef2cd43b91b

src/layers.cpp
@@ -1859,11 +1859,20 @@ void UpdateLayers()
                             {
                                 NPC[B].Location.X = NPC[B].DefaultLocationX;
                                 NPC[B].Location.Y = NPC[B].DefaultLocationY;
+
+                                // In SMBX 1.3, these assignments undone during the next call to UpdateNPCs.
+                                // Add to NPCQueues::Unchecked to ensure that will happen still.
                                 if(NPC[B].Type == NPCID_PLANT_S3 || NPC[B].Type == NPCID_BIG_PLANT || NPC[B].Type == NPCID_PLANT_S1 ||
                                    NPC[B].Type == NPCID_LONG_PLANT_UP || NPC[B].Type == NPCID_FIRE_PLANT)
+                                {
                                     NPC[B].Location.Y += NPC[B]->THeight;
+                                    NPCQueues::Unchecked.push_back(B);
+                                }
                                 else if(NPC[B].Type == NPCID_SIDE_PLANT && NPC[B].Direction == -1)
+                                {
                                     NPC[B].Location.X += NPC[B]->TWidth;
+                                    NPCQueues::Unchecked.push_back(B);
+                                }
                             }
                             else
                             {

src/npc/npc_update.cpp
@@ -975,6 +975,13 @@ interrupt_Activation:
                 NPCHit(A, 9);
             }
         }
+
+        // check for inactive NPCs that got the wrong location
+        if(NPC[A].Hidden)
+        {
+            // essential part of Deactivate(A);
+            NPC[A].ResetLocation();
+        }
     }
 
     NPCQueues::Unchecked.clear();