85ac114b82d7d5d8f2f332c92a1e7174aa13448d
Wohlstand <admin@wohlnet.ru>
2025-02-05 02:29:45 +0300

    Added CMake tool-chain for armhf Linux cross build

---
+++ cmake/ci_linux_gcc_toolchain_armhf.cmake
@@ -0,0 +1,19 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_ASM_COMPILER /usr/bin/arm-linux-gnueabihf-as)
+set(CMAKE_ASM-ATT_COMPILER /usr/bin/arm-linux-gnueabihf-as)
+set(CMAKE_AR ar)
+
+set(CMAKE_SYSTEM_PROCESSOR armhf)
+
+set(CMAKE_CROSSCOMPILING ON)
+
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
