747cf57b6f90e072eb417fcb72f505a6b23c290c
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2025-04-06 03:17:12 +0300

    Improve the mechanism of the exclusive full-screen working (#920) (#938)
    * Revert "frm_main.cpp: temporarily revert forced exclusive fullscreen"
    
    This reverts commit 5e0a4675c3c010d0727d7dc300ad425fbe64e3e8.
    
    * SDL Window: Use slightly different video mode switching
    
    * window_sdl.cpp: Video memory on Windows
    
    This thing will allow to adjust the data better
    
    TODO: Move everything of such into separated CPP file that can contain even more stuff of like to detect system info and use that to adjust defaults.
    
    * Added sketch for the future video modes API
    
    * window_sdl.cpp: Fixed colors list fill
    
    * mememu.cpp: Fixed build
    
    If use D_pLogDebug with no arguments, you should use D_pLogDebugNA equivalent. It's a limitation of variadic macros that may fail to build if no arguments used when supposed.
    
    * window_sdl.cpp: Fixed return
    
    * Add config options for fullscreen resolution and bit depth
    
    * Make resolution switching working
    
    TODO: Fix toggling between desktop and exclusive
    
    * Try to fix switching between desktop and exclusive fullscreen
    
    * Fixed incorrect pixel format returned from bits
    
    * window_sdl.cpp: Init sys info before SDL window
    
    Otherwise, weird crazy behaviour SIGFPE happens...
    
    * render_sdl.cpp: Small style fix
    
    Added space between two IF blocks
    
    * Don't sync full-screen res when it's no fullscreen
    
    * Added small debug-only message
    
    * Make dynamic resolutions lists to work properly
    
    * Added obtained value for colour depth
    
    * Small tweak for the get video ram call
    
    * window_sdl.cpp: Added more debug prints
    
    * Fixed bad arithmetic crash because of bad size return
    
    * window_sdl.cpp: Fixed incorrect depth finding
    
    * Updated AudioCodecs and MixerX
    
    Fixed 3DS build
    
    * Updated AudioCodecs, MixerX, and SDL2
    
    * Attempt to fix the MSVC build
    
    ---------
    
    Co-authored-by: ds-sloth <72112344+ds-sloth@users.noreply.github.com>

--- src/config/config_base.hpp
+++ src/config/config_base.hpp
@@ -62,6 +62,9 @@ template<bool writable>
 class ConfigLanguage_t;
 
 template<bool writable>
+class ConfigFullscreenRes_t;
+
+template<bool writable>
 class ConfigSetupEnum_t;
 
 template<bool writable>
@@ -252,6 +255,22 @@ class ConfigLanguage_t<false> : public ConfigOption_t<false, std::string>
 
 public:
     ConfigLanguage_t(_Config_t<false>* parent,
+        const value_t default_value,
+        const ConfigCompatInfo_t<value_t>& compat_info, uint8_t scope,
+        const char* internal_name, const char* display_name, const char* display_tooltip,
+        void (*onupdate)() = nullptr, bool (*active)() = nullptr, bool (*validate)(void*) = nullptr) :
+            ConfigOption_t<false, value_t>(parent, default_value, compat_info, scope,
+                internal_name, display_name, display_tooltip,
+                onupdate, active, validate)  {}
+};
+
+template<>
+class ConfigFullscreenRes_t<false> : public ConfigOption_t<false, std::pair<int, int>>
+{
+    using value_t = std::pair<int, int>;
+
+public:
+    ConfigFullscreenRes_t(_Config_t<false>* parent,
         const value_t default_value,
         const ConfigCompatInfo_t<value_t>& compat_info, uint8_t scope,
         const char* internal_name, const char* display_name, const char* display_tooltip,
@@ -466,6 +485,34 @@ public:
 
     virtual const std::string& get_display_value(std::string& out) const override;
 };
+
+#ifdef RENDER_FULLSCREEN_TYPES_SUPPORTED
+template<>
+class ConfigFullscreenRes_t<true> : public ConfigOption_t<true, std::pair<int, int>>
+{
+    using value_t = std::pair<int, int>;
+
+public:
+    using BaseConfigOption_t<true>::m_set;
+    using BaseConfigOption_t<true>::m_base;
+    using ConfigOption_t<true, value_t>::operator=;
+
+    ConfigFullscreenRes_t(_Config_t<true>* parent,
+        const value_t default_value,
+        const ConfigCompatInfo_t<value_t>& compat_info, uint8_t scope,
+        const char* internal_name, const char* display_name, const char* display_tooltip,
+        void (*onupdate)() = nullptr, bool (*active)() = nullptr, bool (*validate)(void*) = nullptr) :
+            ConfigOption_t<true, value_t>(parent, default_value, compat_info, scope,
+                internal_name, display_name, display_tooltip,
+                onupdate, active, validate) {}
+
+    size_t find_cur_index();
+
+    virtual bool rotate_left() override;
+    virtual bool rotate_right() override;
+    virtual bool change() override;
+};
+#endif // RENDER_FULLSCREEN_TYPES_SUPPORTED
 
 template<>
 class ConfigSetupEnum_t<true> : public ConfigEnumOption_t<true, int>
--- src/config/config_hooks.cpp
+++ src/config/config_hooks.cpp
@@ -64,12 +64,6 @@ void config_res_set()
     if(GameIsActive && (GameMenu || GamePaused == PauseCode::Options || g_config.internal_res.m_set == ConfigSetLevel::cheat))
         UpdateWindowRes();
     UpdateInternalRes();
-
-#ifdef RENDER_FULLSCREEN_TYPES_SUPPORTED
-    // Sync the real resolution after applying an update
-    if(!XWindow::is_nullptr())
-        XWindow::syncFullScreenRes();
-#endif // RENDER_FULLSCREEN_TYPES_SUPPORTED
 }
 
 void config_rendermode_set()
@@ -102,6 +96,22 @@ void config_fullscreen_set()
 }
 
 #ifdef RENDER_FULLSCREEN_TYPES_SUPPORTED
+void config_video_mode_set()
+{
+    if(!GameIsActive)
+        return;
+
+    AbstractWindow_t::VideoModeRes res;
+    uint8_t depth;
+    res.w = g_config.fullscreen_res.m_value.first;
+    res.h = g_config.fullscreen_res.m_value.second;
+
+    XWindow::setVideoMode(res, g_config.fullscreen_depth);
+
+    XWindow::getCurrentVideoMode(res, depth);
+    g_config.fullscreen_depth.obtained = depth;
+}
+
 void config_fullscreen_type_set()
 {
     if(!GameIsActive)
@@ -191,3 +201,4 @@ void config_compat_changed()
     if(!g_configInTransaction)
         UpdateConfig();
 }
+
