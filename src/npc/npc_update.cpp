2bace9b1f736bc5681895084c44cf789b72d4d61
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2025-04-21 18:19:46 -0400

    npc_update.cpp: don't skip force-activation of generator NPCs
    (cherry picked from commit e228e8a0121f8e6f98950ad53743fb9962eadff5)

--- src/npc/npc_update.cpp
+++ src/npc/npc_update.cpp
@@ -518,7 +518,9 @@ void UpdateNPCs()
                 }
             }
 
-            continue;
+            // skip the rest of this logic unless NPC is a force-active type (in which case the result will be buggy)
+            if(!NPCQueues::check_active_type(NPC[A]))
+                continue;
         }
 
         // Force-active NPCs, part 1
