TheXTech/TheXTech

commit a2ab33bf0e4df0e73540008d1897f3ff4d6cdfbf

Partially replace fmt::format with custom sprintf
(cherry picked from commit 1d909d44114f2b5e305269271248d085485823b0)

src/config/config_impl.hpp
@@ -267,7 +267,7 @@ const std::string& ConfigOption_t<true, std::string>::get_display_value(std::str
 template<>
 const std::string& ConfigOption_t<true, std::pair<int, int>>::get_display_value(std::string& out) const
 {
-    out = fmt::format_ne("{0}x{1}", m_value.first, m_value.second);
+    out = fmt::sprintf_ne("%dx%d", m_value.first, m_value.second);
     return out;
 }
 
@@ -308,7 +308,7 @@ void ConfigOption_t<true, std::pair<int, int>>::save_to_ini(IniProcessing* ini)
 
     if(is_set())
     {
-        std::string out = fmt::format_ne("{0}x{1}", m_value.first, m_value.second);
+        std::string out = fmt::sprintf_ne("%dx%d", m_value.first, m_value.second);
         ini->setValue(m_base->m_internal_name, out);
     }
     else
@@ -319,7 +319,7 @@ void ConfigOption_t<true, std::pair<int, int>>::save_to_ini(IniProcessing* ini)
 template<>
 const std::string& ConfigOption_t<true, std::array<uint8_t, 3>>::get_display_value(std::string& out) const
 {
-    out = fmt::format_ne("{0},{1},{2}", m_value[0], m_value[1], m_value[2]);
+    out = fmt::sprintf_ne("%u,%u,%u", m_value[0], m_value[1], m_value[2]);
     return out;
 }
 
@@ -392,11 +392,11 @@ void ConfigEnumOption_t<false, value_t>::make_translation(XTechTranslate& transl
     if((ConfigOption_t<false, value_t>::m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}._name", cur_section_id, m_internal_name), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s._name", cur_section_id, m_internal_name), &m_display_name});
 
     // some obscure behavior of the GCC version in Ubuntu 16.04 necessitates qualifying m_display_tooltip with the base class scope
     if(!BaseConfigOption_t<false>::m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}._tooltip", cur_section_id, m_internal_name), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s._tooltip", cur_section_id, m_internal_name), &m_display_tooltip});
 
     for(auto& value : m_enum_values)
     {
@@ -404,10 +404,10 @@ void ConfigEnumOption_t<false, value_t>::make_translation(XTechTranslate& transl
             continue;
 
         if(!value.m_display_name.empty())
-            translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}.{2}", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_name});
+            translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s.%s", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_name});
 
         if(!value.m_display_tooltip.empty())
-            translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}.{2}-tip", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_tooltip});
+            translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s.%s-tip", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_tooltip});
     }
 }
 

src/core/opengl/render_gl_frontend.cpp
@@ -309,7 +309,7 @@ bool RenderGL::initRender(SDL_Window *window)
         return false;
     }
 
-    std::string init_string = fmt::format_ne("Initialized OpenGL {0}.{1}-{2}", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
+    std::string init_string = fmt::sprintf_ne("Initialized OpenGL %d.%d-%d", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
 
     init_string += " with draw features: ";
     if(m_use_logicop)

src/editor/editor_custom.cpp
@@ -541,20 +541,20 @@ void ItemList_t::make_translation(XTechTranslate& translate, const char* group, 
         if(indices[i] != -1)
         {
             if(header < 0)
-                translate.m_assetsMap.insert({fmt::format_ne("editor.{0}.auto.{1}{2}", group, prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.auto.%s%d", group, prefix, indices[i]), &names[i]});
             else if(header < (int)families.size())
-                translate.m_assetsMap.insert({fmt::format_ne("editor.{0}.{1}.{2}{3}", group, families[header].second, prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.%s.%d%d", group, families[header].second.c_str(), prefix, indices[i]), &names[i]});
             else
-                translate.m_assetsMap.insert({fmt::format_ne("editor.{0}.fam{1}.{2}{3}", group, header, prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%s.%s%d", group, header, prefix, indices[i]), &names[i]});
         }
         else if(!names[i].empty())
         {
             header++;
 
             if(header < (int)families.size())
-                translate.m_assetsMap.insert({fmt::format_ne("editor.{0}.{1}.header", group, families[header].second), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.%s.header", group, families[header].second.c_str()), &names[i]});
             else
-                translate.m_assetsMap.insert({fmt::format_ne("editor.{0}.fam{1}.header", group, header), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%s.header", group, header), &names[i]});
         }
     }
 }
@@ -1490,14 +1490,14 @@ void Load(XTechTranslate* translate)
     editor.read("any", list_level_exit_names[0], "Any");
     editor.read("none", list_level_exit_names[1], "None");
 
-    translate->m_assetsMap.insert({fmt::format_ne("editor.exit-codes.{0}", "any"), &(list_level_exit_names[0])});
-    translate->m_assetsMap.insert({fmt::format_ne("editor.exit-codes.{0}", "none"), &(list_level_exit_names[1])});
+    translate->m_assetsMap.insert({fmt::sprintf_ne("editor.exit-codes.%s", "any"), &(list_level_exit_names[0])});
+    translate->m_assetsMap.insert({fmt::sprintf_ne("editor.exit-codes.%s", "none"), &(list_level_exit_names[1])});
 
     for(size_t i = 1; i + 1 < list_level_exit_names.size(); i++)
     {
-        const std::string s = fmt::format_ne("code{0}", i);
+        const std::string s = fmt::sprintf_ne("code%zu", i);
         editor.read(s.c_str(), list_level_exit_names[i + 1], s);
-        translate->m_assetsMap.insert({fmt::format_ne("editor.exit-codes.{0}", i), &(list_level_exit_names[i + 1])});
+        translate->m_assetsMap.insert({fmt::sprintf_ne("editor.exit-codes.%zu", i), &(list_level_exit_names[i + 1])});
     }
 
     editor.endGroup();
@@ -1541,7 +1541,7 @@ void Load(XTechTranslate* translate)
     if(translate)
     {
         for(uint8_t i = 0; i < block_families.size(); i++)
-            translate->m_assetsMap.insert({fmt::format_ne("editor.block.{0}", block_family_keys[i]), &(block_families[i].name)});
+            translate->m_assetsMap.insert({fmt::sprintf_ne("editor.block.%s", block_family_keys[i].c_str()), &(block_families[i].name)});
     }
 
 
@@ -1584,7 +1584,7 @@ void Load(XTechTranslate* translate)
     if(translate)
     {
         for(uint8_t i = 0; i < bgo_families.size(); i++)
-            translate->m_assetsMap.insert({fmt::format_ne("editor.bgo.{0}", bgo_family_keys[i]), &(bgo_families[i].name)});
+            translate->m_assetsMap.insert({fmt::sprintf_ne("editor.bgo.%s", bgo_family_keys[i].c_str()), &(bgo_families[i].name)});
     }
 
 
@@ -1630,7 +1630,7 @@ void Load(XTechTranslate* translate)
     if(translate)
     {
         for(uint8_t i = 0; i < npc_families.size(); i++)
-            translate->m_assetsMap.insert({fmt::format_ne("editor.npc.{0}", npc_family_keys[i]), &(npc_families[i].name)});
+            translate->m_assetsMap.insert({fmt::sprintf_ne("editor.npc.%s", npc_family_keys[i].c_str()), &(npc_families[i].name)});
     }
 
 
@@ -1672,7 +1672,7 @@ void Load(XTechTranslate* translate)
     if(translate)
     {
         for(uint8_t i = 0; i < tile_families.size(); i++)
-            translate->m_assetsMap.insert({fmt::format_ne("editor.tile.{0}", tile_family_keys[i]), &(tile_families[i].name)});
+            translate->m_assetsMap.insert({fmt::sprintf_ne("editor.tile.%s", tile_family_keys[i].c_str()), &(tile_families[i].name)});
     }
 
 

src/graphics/gfx_editor.cpp
@@ -781,8 +781,8 @@ void DrawEditorWorld()
                 int at_X = lvl.Location.X + lvl.Location.Width / 2 + camX;
                 int at_Y = lvl.Location.Y + camY - 40;
                 XRender::renderRect(at_X - 80, at_Y - 4, 160, 44, {0, 0, 0, 127}, true);
-                SuperPrintCenter(fmt::format_ne("{0}: {1}", g_editorStrings.letterCoordX, lvl.Location.X), 3, at_X, at_Y);
-                SuperPrintCenter(fmt::format_ne("{0}: {1}", g_editorStrings.letterCoordY, lvl.Location.Y), 3, at_X, at_Y + 20);
+                SuperPrintCenter(fmt::sprintf_ne("%s: %d", g_editorStrings.letterCoordX.c_str(), (int)lvl.Location.X), 3, at_X, at_Y);
+                SuperPrintCenter(fmt::sprintf_ne("%s: 5d", g_editorStrings.letterCoordY.c_str(), (int)lvl.Location.Y), 3, at_X, at_Y + 20);
             }
         }
     }

src/graphics/gfx_hud.cpp
@@ -484,9 +484,9 @@ void DrawMedals(int X, int Y, bool warp, uint8_t max, uint8_t prev, uint8_t ckpt
     int total_len = 0;
 
     if(g_config.medals_show_policy == Config_t::MEDALS_SHOW_COUNTS)
-        label = fmt::format_ne("{0}/{1}", got_count, max);
+        label = fmt::sprintf_ne("%d/%u", got_count, max);
     else
-        label = fmt::format_ne("{0}", got_count);
+        label = fmt::sprintf_ne("%d", got_count);
 
     total_len += coin_width + 8 + GFX.Interface[1].w + 4;
     total_len += SuperTextPixLen(label, 3);

lib/Allocator/PoolAllocator.cpp
@@ -45,10 +45,10 @@ void* PoolAllocator::Allocate(const std::size_t allocationSize, const std::size_
     {
         // Fatal error
         XMsgBox::errorMsgBox("Fatal error",
-            fmt::format_ne("The pool allocator is full:\n"
-            "- Total capacity: {0}\n"
-            "- Current size of free list: {1}\n"
-            "- Maximum size of free list: {2}\n"
+            fmt::sprintf_ne("The pool allocator is full:\n"
+            "- Total capacity: %zu\n"
+            "- Current size of free list: %zu\n"
+            "- Maximum size of free list: %zu\n"
             "\n"
             "Game will be closed. Please check logs for details.",
             m_capacity,

lib/InterProcess/editor_pipe.cpp
@@ -101,7 +101,7 @@ EditorPipe::~EditorPipe()
 
 void EditorPipe::sendStarsNumber(int numStars)
 {
-    sendMessage(fmt::format_ne("CMD:NUM_STARS {0}", numStars));
+    sendMessage(fmt::sprintf_ne("CMD:NUM_STARS %d", numStars));
 }
 
 void EditorPipe::sendTakenBlock(const LevelBlock &block)
@@ -147,12 +147,12 @@ void EditorPipe::sendCloseProperties()
 
 void EditorPipe::sendPlayerSettings(int playerId, int character, int state, int vehicleID, int vehicleState)
 {
-    sendMessage(fmt::format_ne("CMD:PLAYER_SETUP_UPDATE {0} {1} {2} {3} {4}", playerId, character, state, vehicleID, vehicleState));
+    sendMessage(fmt::sprintf_ne("CMD:PLAYER_SETUP_UPDATE %d %d %d %d %d", playerId, character, state, vehicleID, vehicleState));
 }
 
 void EditorPipe::sendPlayerSettings2(int playerId, int health, int reservedItem)
 {
-    sendMessage(fmt::format_ne("CMD:PLAYER_SETUP_UPDATE2 {0} {1} {2}", playerId, health, reservedItem));
+    sendMessage(fmt::sprintf_ne("CMD:PLAYER_SETUP_UPDATE2 %d %d %d", playerId, health, reservedItem));
 }
 
 void EditorPipe::shut()
@@ -216,7 +216,7 @@ void EditorPipe::icomingData(const std::string &in)
         m_doParseLevelData = true;
         m_doAcceptLevelDataParts = false;
         m_acceptedLevel.open(&m_acceptedRawData, m_accepted_lvl_path);
-        IntProc::setState(fmt::format_ne("LVLX is valid: 1"));
+        IntProc::setState("LVLX is valid: 1");
 
         m_levelAccepted_lock.lock();
         m_levelAccepted = true;

src/config/config_base.cpp
@@ -72,10 +72,10 @@ void BaseConfigOption_t<false>::make_translation(XTechTranslate& translate, cons
     if((m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}", cur_section_id, m_internal_name), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s", cur_section_id, m_internal_name), &m_display_name});
 
     if(!m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}-tip", cur_section_id, m_internal_name), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s-tip", cur_section_id, m_internal_name), &m_display_tooltip});
 }
 
 BaseConfigOption_t<true>::BaseConfigOption_t(_Config_t<true>* parent) : m_index(parent->m_options.size())
@@ -127,8 +127,8 @@ void ConfigSection_t<false>::make_translation(XTechTranslate& translate, const c
     if(this != &g_options.compat && (m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}._header", cur_section_id), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s._header", cur_section_id), &m_display_name});
 
     if(!m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}._tooltip", cur_section_id), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s._tooltip", cur_section_id), &m_display_tooltip});
 }

commit a20493e2ad3df711259de1a35f4ab6731fe7bc80

Misc fixes for format strings
(Fixes errors introduced in a2ab33bf)

src/config/config_impl.hpp
@@ -319,7 +319,7 @@ void ConfigOption_t<true, std::pair<int, int>>::save_to_ini(IniProcessing* ini)
 template<>
 const std::string& ConfigOption_t<true, std::array<uint8_t, 3>>::get_display_value(std::string& out) const
 {
-    out = fmt::sprintf_ne("%u,%u,%u", m_value[0], m_value[1], m_value[2]);
+    out = fmt::sprintf_ne("%u,%u,%u", (unsigned)m_value[0], (unsigned)m_value[1], (unsigned)m_value[2]);
     return out;
 }
 

src/core/opengl/render_gl_frontend.cpp
@@ -309,7 +309,7 @@ bool RenderGL::initRender(SDL_Window *window)
         return false;
     }
 
-    std::string init_string = fmt::sprintf_ne("Initialized OpenGL %d.%d-%d", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
+    std::string init_string = fmt::sprintf_ne("Initialized OpenGL %d.%d-%c", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
 
     init_string += " with draw features: ";
     if(m_use_logicop)

src/editor/editor_custom.cpp
@@ -543,11 +543,11 @@ void ItemList_t::make_translation(XTechTranslate& translate, const char* group, 
         if(indices[i] != -1)
         {
             if(header < 0)
-                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.auto.%s%d", group, prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.auto.%s%d", group, prefix, (int)indices[i]), &names[i]});
             else if(header < (int)families.size())
-                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.%s.%s%d", group, families[header].second.c_str(), prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.%s.%s%d", group, families[header].second.c_str(), prefix, (int)indices[i]), &names[i]});
             else
-                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%s.%s%d", group, header, prefix, indices[i]), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%s.%s%d", group, header, prefix, (int)indices[i]), &names[i]});
         }
         else if(!names[i].empty())
         {
@@ -556,7 +556,7 @@ void ItemList_t::make_translation(XTechTranslate& translate, const char* group, 
             if(header < (int)families.size())
                 translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.%s.header", group, families[header].second.c_str()), &names[i]});
             else
-                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%s.header", group, header), &names[i]});
+                translate.m_assetsMap.insert({fmt::sprintf_ne("editor.%s.fam%d.header", group, header), &names[i]});
         }
     }
 }

src/graphics/gfx_editor.cpp
@@ -787,7 +787,7 @@ void DrawEditorWorld()
                 int at_Y = lvl.Location.Y + camY - 40;
                 XRender::renderRect(at_X - 80, at_Y - 4, 160, 44, {0, 0, 0, 127}, true);
                 SuperPrintCenter(fmt::sprintf_ne("%s: %d", g_editorStrings.letterCoordX.c_str(), (int)lvl.Location.X), 3, at_X, at_Y);
-                SuperPrintCenter(fmt::sprintf_ne("%s: 5d", g_editorStrings.letterCoordY.c_str(), (int)lvl.Location.Y), 3, at_X, at_Y + 20);
+                SuperPrintCenter(fmt::sprintf_ne("%s: %d", g_editorStrings.letterCoordY.c_str(), (int)lvl.Location.Y), 3, at_X, at_Y + 20);
             }
         }
     }

src/graphics/gfx_hud.cpp
@@ -497,7 +497,7 @@ void DrawMedals(int X, int Y, bool warp, uint8_t max, uint8_t prev, uint8_t ckpt
     int total_len = 0;
 
     if(g_config.medals_show_policy == Config_t::MEDALS_SHOW_COUNTS)
-        label = fmt::sprintf_ne("%d/%u", got_count, max);
+        label = fmt::sprintf_ne("%d/%u", got_count, (unsigned)max);
     else
         label = fmt::sprintf_ne("%d", got_count);
 