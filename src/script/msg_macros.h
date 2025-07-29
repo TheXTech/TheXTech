27dc822705eb082adf444ca2f51110e8ad2effbf
Wohlstand <admin@wohlnet.ru>
2025-07-30 00:50:02 +0300

    msg_macros: Implemented optional error handling
    (For unit tests)

--- src/script/msg_macros.h
+++ src/script/msg_macros.h
@@ -24,12 +24,47 @@
 #include <string>
 #ifdef MOONDUST_UNIT_TEST
 #   include <vector>
+#   ifndef MOONDUST_MSG_MACROS_ERROR_HANDLING
+#       define MOONDUST_MSG_MACROS_ERROR_HANDLING
+#   endif
+#endif
+
+#ifdef MOONDUST_MSG_MACROS_ERROR_HANDLING
+enum MsgMacroErrors
+{
+    MSG_MACRO_ERROR_OK = 0,
+    MSG_MACRO_ERROR_BAD_CMD_SYNTAX,
+    MSG_MACRO_ERROR_BAD_FUNC_SYNTAX,
+    MSG_MACRO_ERROR_UNKNOWN_CMD,
+    MSG_MACRO_ERROR_UNKNOWN_FUNC,
+    MSG_MACRO_ERROR_BAD_FUNC_ARGS,
+    MSG_MACRO_ERROR_EXTRA_SYMBOLS_AT_END,
+};
 #endif
 
 #ifdef MOONDUST_UNIT_TEST
 extern bool msgMacroParseTokens(const std::string &line, std::vector<std::string> &tokens);
 #endif
 
+#ifdef MOONDUST_MSG_MACROS_ERROR_HANDLING
+/**
+ * @brief Pre-Process the message (apply conditions, or place player names where is possible)
+ * @param in Input text to pre-process
+ * @param out Output text of pre-processing result
+ * @param macro_player ID of playable character to match the condition
+ * @param macro_state ID of character state to match the condition
+ * @param error Optional parse error
+ */
+extern void msgMacroProcess(const std::string &in, std::string &out, int macro_player, int macro_state, MsgMacroErrors *error = nullptr);
+#else // MOONDUST_MSG_MACROS_ERROR_HANDLING
+/**
+ * @brief Pre-Process the message (apply conditions, or place player names where is possible)
+ * @param in Input text to pre-process
+ * @param out Output text of pre-processing result
+ * @param macro_player ID of playable character to match the condition
+ * @param macro_state ID of character state to match the condition
+ */
 extern void msgMacroProcess(const std::string &in, std::string &out, int macro_player, int macro_state);
+#endif // MOONDUST_MSG_MACROS_ERROR_HANDLING
 
 #endif // MSGMACROS_H
