TheXTech/TheXTech

commit f3c1fc18b3dc2fb373d86a7fa02bb9a5eb46ff8b

Process drop/add through XMessage system

src/message.cpp
@@ -62,6 +62,39 @@ void Handle(const Message& m)
         if(LevelSelect)
             SetupPlayers();
     }
+    else if(m.type == Type::add_player || m.type == Type::add_player_dead)
+    {
+        if(m.screen >= maxNetplayClients || Screens[m.screen].player_count >= maxLocalPlayers || m.message < 1 || m.message > numCharacters)
+            return;
+
+        // after AddPlayer, numPlayers is always the new player
+        AddPlayer(m.message, Screens[m.screen]);
+
+        // set the player to be dead if needed
+        if(m.type == Type::add_player_dead)
+        {
+            Player[numPlayers].Dead = true;
+
+            // initialize ghost logic for player
+            int living = CheckNearestLiving(numPlayers);
+            if(living)
+            {
+                Player[numPlayers].Effect2 = -living;
+                Player[numPlayers].Location.X = Player[living].Location.X;
+                Player[numPlayers].Location.Y = Player[living].Location.Y;
+                Player[numPlayers].Section    = Player[living].Section;
+            }
+            else
+                Player[numPlayers].Effect2 = 0;
+        }
+    }
+    else if(m.type == Type::drop_player)
+    {
+        if(m.screen >= maxNetplayClients || m.player >= Screens[m.screen].player_count)
+            return;
+
+        DropPlayer(Screens[m.screen].players[m.player]);
+    }
     else if(m.type == Type::menu_action)
     {
         PauseScreen::g_pending_action = m.message;

src/main/screen_connect.cpp
@@ -3,6 +3,7 @@
 #include "core/render.h"
 
 #include "global_constants.h"
+#include "message.h"
 #include "controls.h"
 #include "sound.h"
 #include "globals.h"
@@ -633,7 +634,7 @@ static void Player_Remove(int p)
         if((Player[l_screen->players[p]].Dead || Player[l_screen->players[p]].TimeToLive > 0) && Player[l_screen->players[p]].Effect != PLREFF_COOP_WINGS)
             s_char_info.dead_count++;
 
-        DropPlayer(l_screen->players[p]);
+        XMessage::PushMessage({XMessage::Type::drop_player, (uint8_t)p, 0});
     }
 }
 
@@ -683,7 +684,7 @@ void PlayerBox::UpdatePlayer()
     if(p < l_screen->player_count)
     {
         if(l_screen->charSelect[p] != Player[l_screen->players[p]].Character)
-            SwapCharacter(l_screen->players[p], l_screen->charSelect[p]);
+            XMessage::PushMessage({XMessage::Type::char_swap, (uint8_t)p, (uint8_t)l_screen->charSelect[p]});
     }
     // otherwise, add new player!
     else
@@ -691,32 +692,20 @@ void PlayerBox::UpdatePlayer()
         // swap p with the first non-existent player slot
         Player_Swap(l_screen->player_count, p);
 
-        // after AddPlayer, numPlayers is always the new player
-        AddPlayer(l_screen->charSelect[l_screen->player_count], *l_screen);
+        // check the desired character
+        uint8_t chara = l_screen->charSelect[l_screen->player_count];
 
         // add as dead if dead player was dropped in this level
         if(s_char_info.dead_count > 0)
         {
             s_char_info.dead_count--;
-            Player[numPlayers].Dead = true;
-
-            // initialize ghost logic for player
-            int living = CheckLiving();
-            if(living)
-            {
-                Player[numPlayers].Effect2 = -living;
-                Player[numPlayers].Location.X = Player[living].Location.X;
-                Player[numPlayers].Location.Y = Player[living].Location.Y;
-                Player[numPlayers].Section    = Player[living].Section;
-            }
-            else
-                Player[numPlayers].Effect2 = 0;
-
             PlaySound(SFX_ShellHit);
+            XMessage::PushMessage({XMessage::Type::add_player_dead, 0, chara});
         }
         else
         {
             PlaySound(SFX_DropItem);
+            XMessage::PushMessage({XMessage::Type::add_player, 0, chara});
         }
     }
 }

src/main/screen_pause.cpp
@@ -706,13 +706,7 @@ void ControlsLogic()
                     if(target != Player[A].Character)
                     {
                         snd = SFX_Slide;
-
-                        XMessage::Message swap_char;
-                        swap_char.type = XMessage::Type::char_swap;
-                        swap_char.player = plr_i;
-                        swap_char.message = target;
-
-                        XMessage::PushMessage(swap_char);
+                        XMessage::PushMessage({XMessage::Type::char_swap, plr_i, target});
                     }
                 }
 

src/message.h
@@ -44,6 +44,9 @@ enum class Type : uint8_t
     release,
     menu_action,
     char_swap,
+    add_player,
+    add_player_dead,
+    drop_player,
     shared_controls,
     enter_code,
 };

commit 4ae9b91b13199e286092275379f46d8bd3468cd3

message.cpp: don't invalidly call SetupPlayers at menu
Fixes main menu bug introduced in f3c1fc18b3dc2fb373d86a7fa02bb9a5eb46ff8b where dead mode was interrupted when choosing a character

src/message.cpp
@@ -65,7 +65,7 @@ void Handle(const Message& m)
 
         SwapCharacter(screen.players[m.player], m.message);
 
-        if(LevelSelect)
+        if(LevelSelect && !GameMenu)
             SetupPlayers();
     }
     else if(m.type == Type::add_player || m.type == Type::add_player_dead)