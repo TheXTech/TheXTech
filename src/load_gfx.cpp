TheXTech/TheXTech

commit a2ab33bf0e4df0e73540008d1897f3ff4d6cdfbf

Partially replace fmt::format with custom sprintf
(cherry picked from commit 1d909d44114f2b5e305269271248d085485823b0)

src/load_gfx.cpp
@@ -857,11 +857,11 @@ void LoadGFX()
     LoaderUpdateDebugString("Characters");
     for(int c = 0; c < numCharacters; ++c)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Character {0}", c));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Character %d", c));
         CurDir.setCurDir(getGfxDir() + GFXPlayerNames[c]);
         For(A, 1, numStates)
         {
-            s_find_image(p, CurDir, fmt::format_ne("{1}-{0}", A, GFXPlayerNames[c]));
+            s_find_image(p, CurDir, fmt::sprintf_ne("%s-%d", GFXPlayerNames[c], A));
             if(!p.empty())
                 XRender::lazyLoadPicture((*GFXCharacterBMP[c])[A], p);
         }
@@ -876,8 +876,8 @@ void LoadGFX()
         if(A == BLKID_CONVEYOR_L_CONV || A == BLKID_CONVEYOR_R_CONV)
             continue;
 
-        LoaderUpdateDebugString(fmt::format_ne("Block {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("block-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Block %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("block-%d", A));
 
         if(!p.empty())
             XRender::lazyLoadPicture(GFXBlockBMP[A], p);
@@ -894,8 +894,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "background2/");
     for(int A = 1; A <= numBackground2; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Background {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("background2-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Background %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("background2-%d", A));
 
         if(!p.empty())
             XRender::lazyLoadPicture(GFXBackground2BMP[A], p);
@@ -912,8 +912,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "npc/");
     for(int A = 1; A <= maxNPCType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("NPC {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("npc-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("NPC %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("npc-%d", A));
 
         if(!p.empty())
         {
@@ -945,8 +945,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "effect/");
     for(int A = 1; A <= maxEffectType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Effect {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("effect-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Effect %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("effect-%d", A));
 
         if(!p.empty())
         {
@@ -964,8 +964,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "yoshi/");
     for(int A = 1; A <= maxYoshiGfx; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Mount B {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("yoshib-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Mount B %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("yoshib-%d", A));
 
         if(!p.empty())
         {
@@ -982,8 +982,8 @@ void LoadGFX()
 
     for(int A = 1; A <= maxYoshiGfx; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Mount T {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("yoshit-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Mount T %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("yoshit-%d", A));
 
         if(!p.empty())
         {
@@ -1003,8 +1003,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "background/");
     for(int A = 1; A <= maxBackgroundType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("BGO {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("background-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("BGO %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("background-%d", A));
 
         if(!p.empty())
         {
@@ -1029,8 +1029,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "tile/");
     for(int A = 1; A <= maxTileType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Terrain {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("tile-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Terrain %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("tile-%d", A));
         if(!p.empty())
         {
             XRender::lazyLoadPicture(GFXTileBMP[A], p);
@@ -1049,8 +1049,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "level/");
     for(int A = 0; A <= maxLevelType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Level {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("level-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Level %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("level-%d", A));
         if(!p.empty())
         {
             XRender::lazyLoadPicture(GFXLevelBMP[A], p);
@@ -1069,8 +1069,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "scene/");
     for(int A = 1; A <= maxSceneType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Scenery {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("scene-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Scenery %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("scene-%d", A));
         if(!p.empty())
         {
             XRender::lazyLoadPicture(GFXSceneBMP[A], p);
@@ -1089,8 +1089,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "player/");
     for(int A = 1; A <= numCharacters; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("World character {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("player-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("World character %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("player-%d", A));
         if(!p.empty())
         {
             XRender::lazyLoadPicture(GFXPlayerBMP[A], p);
@@ -1109,8 +1109,8 @@ void LoadGFX()
     CurDir.setCurDir(getGfxDir() + "path/");
     for(int A = 1; A <= maxPathType; ++A)
     {
-        LoaderUpdateDebugString(fmt::format_ne("Path cell {0}", A));
-        s_find_image(p, CurDir, fmt::format_ne("path-{0}", A));
+        LoaderUpdateDebugString(fmt::sprintf_ne("Path cell %d", A));
+        s_find_image(p, CurDir, fmt::sprintf_ne("path-%d", A));
         if(!p.empty())
         {
             XRender::lazyLoadPicture(GFXPathBMP[A], p);
@@ -1201,13 +1201,13 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.BMWin, false, true);
 
     For(i, 1, 3)
-        loadCGFX(uiRoot + fmt::format_ne("Boot{0}.png", i),
-                 fmt::format_ne("Boot{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Boot%d.png", i),
+                 fmt::sprintf_ne("Boot%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Boot[i], false, true);
 
     For(i, 1, 5)
-        loadCGFX(uiRoot + fmt::format_ne("CharacterName{0}.png", i),
-                 fmt::format_ne("CharacterName{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("CharacterName%d.png", i),
+                 fmt::sprintf_ne("CharacterName%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.CharacterName[i], false, true);
 
     loadCGFX(uiRoot + "Chat.png",
@@ -1215,23 +1215,23 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.Chat, false, true);
 
     For(i, 0, 2)
-        loadCGFX(uiRoot + fmt::format_ne("Container{0}.png", i),
-                 fmt::format_ne("Container{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Container%d.png", i),
+                 fmt::sprintf_ne("Container%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Container[i], false, true);
 
     For(i, 1, 3)
-        loadCGFX(uiRoot + fmt::format_ne("ECursor{0}.png", i),
-                 fmt::format_ne("ECursor{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("ECursor%d.png", i),
+                 fmt::sprintf_ne("ECursor%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.ECursor[i], false, true);
 
     For(i, 0, 9)
-        loadCGFX(uiRoot + fmt::format_ne("Font1_{0}.png", i),
-                 fmt::format_ne("Font1_{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Font1_%d.png", i),
+                 fmt::sprintf_ne("Font1_%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Font1[i], false, true);
 
     For(i, 1, 3)
-        loadCGFX(uiRoot + fmt::format_ne("Font2_{0}.png", i),
-                 fmt::format_ne("Font2_{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Font2_%d.png", i),
+                 fmt::sprintf_ne("Font2_%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Font2[i], false, true);
 
     loadCGFX(uiRoot + "Font2S.png",
@@ -1239,13 +1239,13 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.Font2S, false, true);
 
     For(i, 1, 2)
-        loadCGFX(uiRoot + fmt::format_ne("Heart{0}.png", i),
-                 fmt::format_ne("Heart{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Heart%d.png", i),
+                 fmt::sprintf_ne("Heart%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Heart[i], false, true);
 
     For(i, 0, 8)
-        loadCGFX(uiRoot + fmt::format_ne("Interface{0}.png", i),
-                 fmt::format_ne("Interface{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Interface%d.png", i),
+                 fmt::sprintf_ne("Interface%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Interface[i], false, true);
 
     loadCGFX(uiRoot + "LoadCoin.png",
@@ -1257,13 +1257,13 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.Loader, false, true);
 
     For(i, 0, 3)
-        loadCGFX(uiRoot + fmt::format_ne("MCursor{0}.png", i),
-                 fmt::format_ne("MCursor{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("MCursor%d.png", i),
+                 fmt::sprintf_ne("MCursor%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.MCursor[i], false, true);
 
     For(i, 1, 4)
-        loadCGFX(uiRoot + fmt::format_ne("MenuGFX{0}.png", i),
-                 fmt::format_ne("MenuGFX{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("MenuGFX%d.png", i),
+                 fmt::sprintf_ne("MenuGFX%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.MenuGFX[i], false, true);
 
     loadCGFX(uiRoot + "Mount.png",
@@ -1271,8 +1271,8 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.Mount[2], false, true);
 
     For(i, 0, 7)
-        loadCGFX(uiRoot + fmt::format_ne("nCursor{0}.png", i),
-                 fmt::format_ne("nCursor{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("nCursor%d.png", i),
+                 fmt::sprintf_ne("nCursor%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.nCursor[i], false, true);
 
     loadCGFX(uiRoot + "TextBox.png",
@@ -1280,8 +1280,8 @@ static void loadCustomUIAssets()
              nullptr, nullptr, &GFX.isCustom(ci++), GFX.TextBox, false, true);
 
     For(i, 1, 2)
-        loadCGFX(uiRoot + fmt::format_ne("Tongue{0}.png", i),
-                 fmt::format_ne("Tongue{0}", i),
+        loadCGFX(uiRoot + fmt::sprintf_ne("Tongue%d.png", i),
+                 fmt::sprintf_ne("Tongue%d", i),
                  nullptr, nullptr, &GFX.isCustom(ci++), GFX.Tongue[i], false, true);
 
     // loadCGFX(uiRoot + "Warp.png",
@@ -1389,15 +1389,15 @@ void LoadCustomGFX(bool include_world, const char* preview_players_from)
 
     for(int A = 1; A <= maxYoshiGfx; ++A)
     {
-        loadCGFX(GfxRoot + fmt::format_ne("yoshi/yoshib-{0}.png", A),
-                 fmt::format_ne("yoshib-{0}", A),
+        loadCGFX(GfxRoot + fmt::sprintf_ne("yoshi/yoshib-%d.png", A),
+                 fmt::sprintf_ne("yoshib-%d", A),
                  nullptr, nullptr, nullptr, GFXYoshiBBMP[A]);
     }
 
     for(int A = 1; A <= maxYoshiGfx; ++A)
     {
-        loadCGFX(GfxRoot + fmt::format_ne("yoshi/yoshit-{0}.png", A),
-                 fmt::format_ne("yoshit-{0}", A),
+        loadCGFX(GfxRoot + fmt::sprintf_ne("yoshi/yoshit-%d.png", A),
+                 fmt::sprintf_ne("yoshit-%d", A),
                  nullptr, nullptr, nullptr, GFXYoshiTBMP[A]);
     }
 
@@ -1405,7 +1405,7 @@ void LoadCustomGFX(bool include_world, const char* preview_players_from)
     {
         for(int A = 1; A <= numStates; ++A)
         {
-            loadCGFX(GfxRoot + fmt::format_ne("{1}/{1}-{0}.png", A, GFXPlayerNames[c]),
+            loadCGFX(GfxRoot + fmt::sprintf_ne("{1}/{1}-{0}.png", A, GFXPlayerNames[c]),
                      fmt::format_ne("{1}-{0}", A, GFXPlayerNames[c]),
                      nullptr, nullptr,
                      nullptr, (*GFXCharacterBMP[c])[A]);

lib/Allocator/PoolAllocator.cpp
@@ -45,10 +45,10 @@ void* PoolAllocator::Allocate(const std::size_t allocationSize, const std::size_
     {
         // Fatal error
         XMsgBox::errorMsgBox("Fatal error",
-            fmt::format_ne("The pool allocator is full:\n"
-            "- Total capacity: {0}\n"
-            "- Current size of free list: {1}\n"
-            "- Maximum size of free list: {2}\n"
+            fmt::sprintf_ne("The pool allocator is full:\n"
+            "- Total capacity: %zu\n"
+            "- Current size of free list: %zu\n"
+            "- Maximum size of free list: %zu\n"
             "\n"
             "Game will be closed. Please check logs for details.",
             m_capacity,

lib/InterProcess/editor_pipe.cpp
@@ -101,7 +101,7 @@ EditorPipe::~EditorPipe()
 
 void EditorPipe::sendStarsNumber(int numStars)
 {
-    sendMessage(fmt::format_ne("CMD:NUM_STARS {0}", numStars));
+    sendMessage(fmt::sprintf_ne("CMD:NUM_STARS %d", numStars));
 }
 
 void EditorPipe::sendTakenBlock(const LevelBlock &block)
@@ -147,12 +147,12 @@ void EditorPipe::sendCloseProperties()
 
 void EditorPipe::sendPlayerSettings(int playerId, int character, int state, int vehicleID, int vehicleState)
 {
-    sendMessage(fmt::format_ne("CMD:PLAYER_SETUP_UPDATE {0} {1} {2} {3} {4}", playerId, character, state, vehicleID, vehicleState));
+    sendMessage(fmt::sprintf_ne("CMD:PLAYER_SETUP_UPDATE %d %d %d %d %d", playerId, character, state, vehicleID, vehicleState));
 }
 
 void EditorPipe::sendPlayerSettings2(int playerId, int health, int reservedItem)
 {
-    sendMessage(fmt::format_ne("CMD:PLAYER_SETUP_UPDATE2 {0} {1} {2}", playerId, health, reservedItem));
+    sendMessage(fmt::sprintf_ne("CMD:PLAYER_SETUP_UPDATE2 %d %d %d", playerId, health, reservedItem));
 }
 
 void EditorPipe::shut()
@@ -216,7 +216,7 @@ void EditorPipe::icomingData(const std::string &in)
         m_doParseLevelData = true;
         m_doAcceptLevelDataParts = false;
         m_acceptedLevel.open(&m_acceptedRawData, m_accepted_lvl_path);
-        IntProc::setState(fmt::format_ne("LVLX is valid: 1"));
+        IntProc::setState("LVLX is valid: 1");
 
         m_levelAccepted_lock.lock();
         m_levelAccepted = true;

src/config/config_base.cpp
@@ -72,10 +72,10 @@ void BaseConfigOption_t<false>::make_translation(XTechTranslate& translate, cons
     if((m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}", cur_section_id, m_internal_name), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s", cur_section_id, m_internal_name), &m_display_name});
 
     if(!m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}-tip", cur_section_id, m_internal_name), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s-tip", cur_section_id, m_internal_name), &m_display_tooltip});
 }
 
 BaseConfigOption_t<true>::BaseConfigOption_t(_Config_t<true>* parent) : m_index(parent->m_options.size())
@@ -127,8 +127,8 @@ void ConfigSection_t<false>::make_translation(XTechTranslate& translate, const c
     if(this != &g_options.compat && (m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}._header", cur_section_id), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s._header", cur_section_id), &m_display_name});
 
     if(!m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}._tooltip", cur_section_id), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s._tooltip", cur_section_id), &m_display_tooltip});
 }

src/config/config_impl.hpp
@@ -267,7 +267,7 @@ const std::string& ConfigOption_t<true, std::string>::get_display_value(std::str
 template<>
 const std::string& ConfigOption_t<true, std::pair<int, int>>::get_display_value(std::string& out) const
 {
-    out = fmt::format_ne("{0}x{1}", m_value.first, m_value.second);
+    out = fmt::sprintf_ne("%dx%d", m_value.first, m_value.second);
     return out;
 }
 
@@ -308,7 +308,7 @@ void ConfigOption_t<true, std::pair<int, int>>::save_to_ini(IniProcessing* ini)
 
     if(is_set())
     {
-        std::string out = fmt::format_ne("{0}x{1}", m_value.first, m_value.second);
+        std::string out = fmt::sprintf_ne("%dx%d", m_value.first, m_value.second);
         ini->setValue(m_base->m_internal_name, out);
     }
     else
@@ -319,7 +319,7 @@ void ConfigOption_t<true, std::pair<int, int>>::save_to_ini(IniProcessing* ini)
 template<>
 const std::string& ConfigOption_t<true, std::array<uint8_t, 3>>::get_display_value(std::string& out) const
 {
-    out = fmt::format_ne("{0},{1},{2}", m_value[0], m_value[1], m_value[2]);
+    out = fmt::sprintf_ne("%u,%u,%u", m_value[0], m_value[1], m_value[2]);
     return out;
 }
 
@@ -392,11 +392,11 @@ void ConfigEnumOption_t<false, value_t>::make_translation(XTechTranslate& transl
     if((ConfigOption_t<false, value_t>::m_scope & Options_t::Scope::MakeTranslation) == 0)
         return;
 
-    translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}._name", cur_section_id, m_internal_name), &m_display_name});
+    translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s._name", cur_section_id, m_internal_name), &m_display_name});
 
     // some obscure behavior of the GCC version in Ubuntu 16.04 necessitates qualifying m_display_tooltip with the base class scope
     if(!BaseConfigOption_t<false>::m_display_tooltip.empty())
-        translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}._tooltip", cur_section_id, m_internal_name), &m_display_tooltip});
+        translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s._tooltip", cur_section_id, m_internal_name), &m_display_tooltip});
 
     for(auto& value : m_enum_values)
     {
@@ -404,10 +404,10 @@ void ConfigEnumOption_t<false, value_t>::make_translation(XTechTranslate& transl
             continue;
 
         if(!value.m_display_name.empty())
-            translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}.{2}", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_name});
+            translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s.%s", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_name});
 
         if(!value.m_display_tooltip.empty())
-            translate.m_engineMap.insert({fmt::format_ne("menu.options.{0}.{1}.{2}-tip", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_tooltip});
+            translate.m_engineMap.insert({fmt::sprintf_ne("menu.options.%s.%s.%s-tip", cur_section_id, m_internal_name, value.m_internal_name), &value.m_display_tooltip});
     }
 }
 

src/core/language/language_common.cpp
@@ -69,8 +69,8 @@ static bool detectSetup()
 
     if(!CurrentLangDialect.empty())
     {
-        langEngineFile = AppPathManager::languagesDir() + fmt::format_ne("thextech_{0}-{1}.json", CurrentLanguage.c_str(), CurrentLangDialect.c_str());
-        langAssetsFile = AppPathManager::languagesDir() + fmt::format_ne("assets_{0}-{1}.json", CurrentLanguage.c_str(), CurrentLangDialect.c_str());
+        langEngineFile = AppPathManager::languagesDir() + fmt::sprintf_ne("thextech_%s-%s.json", CurrentLanguage.c_str(), CurrentLangDialect.c_str());
+        langAssetsFile = AppPathManager::languagesDir() + fmt::sprintf_ne("assets_%s-%s.json", CurrentLanguage.c_str(), CurrentLangDialect.c_str());
         if(!Files::fileExists(langEngineFile))
             langEngineFile.clear();
         if(!Files::fileExists(langAssetsFile))
@@ -78,9 +78,9 @@ static bool detectSetup()
     }
 
     if(langEngineFile.empty())
-        langEngineFile = AppPathManager::languagesDir() + fmt::format_ne("thextech_{0}.json", CurrentLanguage.c_str());
+        langEngineFile = AppPathManager::languagesDir() + fmt::sprintf_ne("thextech_%s.json", CurrentLanguage.c_str());
     if(langAssetsFile.empty())
-        langAssetsFile = AppPathManager::languagesDir() + fmt::format_ne("assets_{0}.json", CurrentLanguage.c_str());
+        langAssetsFile = AppPathManager::languagesDir() + fmt::sprintf_ne("assets_%s.json", CurrentLanguage.c_str());
 
     if(!Files::fileExists(langEngineFile))
         langEngineFile.clear();
@@ -259,7 +259,7 @@ void XLanguage::rotateLanguage(std::string& nextLanguage, int step)
     }
 
     // find current filename
-    std::string seek_fn = fmt::format_ne("thextech_{0}.json", (const std::string&) g_config.language);
+    std::string seek_fn = fmt::format_ne("thextech_{0}.json", (const std::string&)g_config.language);
 
     auto curr_lang_fn = std::find(s_languages.cbegin(), s_languages.cend(), seek_fn);
 

src/core/opengl/render_gl_frontend.cpp
@@ -309,7 +309,7 @@ bool RenderGL::initRender(SDL_Window *window)
         return false;
     }
 
-    std::string init_string = fmt::format_ne("Initialized OpenGL {0}.{1}-{2}", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
+    std::string init_string = fmt::sprintf_ne("Initialized OpenGL %d.%d-%d", m_gl_majver, m_gl_minver, get_profile_letter(m_gl_profile));
 
     init_string += " with draw features: ";
     if(m_use_logicop)

src/custom.cpp
@@ -97,7 +97,7 @@ void SavePlayerDefaults()
         for(int S = 1; S <= numStates; ++S)
         {
             // Global override of player setup
-            PlayerPathRes = PlayerDir.resolveFileCaseExistsAbs(fmt::format_ne("{0}-{1}.ini", s_playerFileName[C], S));
+            PlayerPathRes = PlayerDir.resolveFileCaseExistsAbs(fmt::sprintf_ne("%s-%d.ini", s_playerFileName[C], S));
             if(!PlayerPathRes.empty())
                 LoadCustomPlayer(C, S, PlayerPathRes);
         }
@@ -180,7 +180,7 @@ void SaveNPCDefaults()
     for(int A = 1; A <= maxNPCType; A++)
     {
         // Global override of NPC setup
-        npcPathRes = NPCDir.resolveFileCaseExistsAbs(fmt::format_ne("npc-{0}.txt", A));
+        npcPathRes = NPCDir.resolveFileCaseExistsAbs(fmt::sprintf_ne("npc-%d.txt", A));
 
         if(!npcPathRes.empty())
             LoadCustomNPC(A, npcPathRes);
@@ -221,12 +221,13 @@ void FindCustomPlayers(const char* preview_players_from)
     {
         for(int S = 1; S <= numStates; ++S)
         {
+            const auto pFile = fmt::sprintf_ne("%s-%d.ini", s_playerFileName[C], S);
             // Episode-wide custom player setup
-            playerPath = g_dirEpisode.resolveFileCaseExistsAbs(fmt::format_ne("{1}-{0}.ini", S, s_playerFileName[C]));
+            playerPath = g_dirEpisode.resolveFileCaseExistsAbs(pFile);
 
             // Level-wide custom player setup
             if(!preview_players_from)
-                playerPathC = g_dirCustom.resolveFileCaseExistsAbs(fmt::format_ne("{1}-{0}.ini", S, s_playerFileName[C]));
+                playerPathC = g_dirCustom.resolveFileCaseExistsAbs(pFile);
 
             if(!playerPath.empty())
                 LoadCustomPlayer(C, S, playerPath);
@@ -343,10 +344,11 @@ void FindCustomNPCs(/*std::string cFilePath*/)
 
     for(int A = 1; A < maxNPCType; ++A)
     {
+        const auto nFile = fmt::sprintf_ne("npc-%d.txt", A);
         // Episode-wide custom NPC setup
-        npcPath = g_dirEpisode.resolveFileCaseExistsAbs(fmt::format_ne("npc-{0}.txt", A));
+        npcPath = g_dirEpisode.resolveFileCaseExistsAbs(nFile);
         // Level-wide custom NPC setup
-        npcPathC = g_dirCustom.resolveFileCaseExistsAbs(fmt::format_ne("npc-{0}.txt", A));
+        npcPathC = g_dirCustom.resolveFileCaseExistsAbs(nFile);
 
         if(!npcPath.empty())
             LoadCustomNPC(A, npcPath);

commit cd3711b7232495a27e3eeb95d5bc6c49546795cb

load_gfx.cpp: fix 1.3.7.1 bug where backup player masks were not loaded
Introduced to main in a2ab33bf0e4df0e73540008d1897f3ff4d6cdfbf
Introduced to stable-1.3.7.x in 1d909d44114f2b5e305269271248d085485823b0

src/load_gfx.cpp
@@ -1410,8 +1410,8 @@ void LoadCustomGFX(bool include_world, const char* preview_players_from)
     {
         for(int A = 1; A <= numStates; ++A)
         {
-            loadCGFX(GfxRoot + fmt::sprintf_ne("{1}/{1}-{0}.png", A, GFXPlayerNames[c]),
-                     fmt::format_ne("{1}-{0}", A, GFXPlayerNames[c]),
+            loadCGFX(GfxRoot + fmt::sprintf_ne("%s/%s-%d.png", GFXPlayerNames[c], GFXPlayerNames[c], A),
+                     fmt::sprintf_ne("%s-%d", GFXPlayerNames[c], A),
                      nullptr, nullptr,
                      nullptr, (*GFXCharacterBMP[c])[A]);
         }