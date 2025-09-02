a328d8442549e8b4df5035f9ff7cae2fd13644b6
Wohlstand <admin@wohlnet.ru>
2025-09-03 02:53:31 +0300

    Android: Updated Gradle, and tweaked the build
    - Updated Gradle
    - Enable the support for 16KB memorz page size (I will need to test this on Android 4 devices just in a case)

--- android-project/thextech/src/main/java/ru/wohlsoft/thextech/IniFile.java
+++ android-project/thextech/src/main/java/ru/wohlsoft/thextech/IniFile.java
@@ -31,50 +31,22 @@ public class IniFile
             BufferedReader br = new BufferedReader(new FileReader(path));
             String line;
             String section = null;
+
             while((line = br.readLine()) != null)
             {
                 Matcher m = _section.matcher(line);
+
                 if(m.matches())
                 {
-                    section = m.group(1).trim();
+                    section = m.group(1);
+                    if(section != null)
+                        section = section.trim();
                 }
                 else if(section != null)
                 {
                     m = _keyValue.matcher(line);
                     if(m.matches())
-                    {
-                        String key = m.group(1).trim();
-                        String value = m.group(2).trim();
-
-                        Boolean quoteOpen = false;
-                        for(int i = 0; i < value.length(); ++i)
-                        {
-                            char c = value.charAt(i);
-                            if(!quoteOpen && c == '"')
-                            {
-                                quoteOpen = true;
-                            }
-                            else if(quoteOpen && c == '"')
-                            {
-                                quoteOpen = false;
-                            }
-                            else if(!quoteOpen && c == ';')
-                            {
-                                value = value.substring(0, i).trim();
-                                break; // Found leading comment, let's strip it!
-                            }
-                        }
-
-                        if(value.startsWith("\"") && value.endsWith("\""))
-                            value = value.substring(1, value.length()-1).trim();
-
-                        Map<String, String> kv = _entries.get(section);
-
-                        if(kv == null)
-                            _entries.put(section, kv = new HashMap<>());
-
-                        kv.put(key, value);
-                    }
+                        insertValue(section, m);
                 }
             }
         }
@@ -82,6 +54,47 @@ public class IniFile
         {
             e.printStackTrace();
         }
+    }
+
+    private void insertValue(String section, Matcher m)
+    {
+        String key = m.group(1);
+        String value = m.group(2);
+
+        if(key == null || value == null)
+            return;
+
+        key = key.trim();
+        value = value.trim();
+
+        boolean quoteOpen = false;
+        for(int i = 0; i < value.length(); ++i)
+        {
+            char c = value.charAt(i);
+            if(!quoteOpen && c == '"')
+            {
+                quoteOpen = true;
+            }
+            else if(quoteOpen && c == '"')
+            {
+                quoteOpen = false;
+            }
+            else if(!quoteOpen && c == ';')
+            {
+                value = value.substring(0, i).trim();
+                break; // Found leading comment, let's strip it!
+            }
+        }
+
+        if(value.startsWith("\"") && value.endsWith("\""))
+            value = value.substring(1, value.length()-1).trim();
+
+        Map<String, String> kv = _entries.get(section);
+
+        if(kv == null)
+            _entries.put(section, kv = new HashMap<>());
+
+        kv.put(key, value);
     }
 
     public String getString(String section, String key, String defaultvalue)
