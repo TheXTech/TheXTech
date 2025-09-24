110638d2d18c47d7935b81e0e507a863f118e79e
Wohlstand <admin@wohlnet.ru>
2025-09-24 22:52:37 +0300

    Adjusted audio setup for PSP

--- src/sound.cpp
+++ src/sound.cpp
@@ -97,6 +97,13 @@ const AudioDefaults_t g_audioDefaults =
     1024,
     (int)AUDIO_S16SYS
 };
+#elif defined(__PSP__)
+{
+    22050,
+    2,
+    1024,
+    (int)AUDIO_S16SYS
+};
 #else /* Defaults for all other platforms */
 {
     44100,
@@ -207,8 +214,10 @@ static void extSfxStopCallback(int channel);
 
 static const int maxSfxChannels = 91;
 
-#ifdef LOW_MEM
+#if defined(LOW_MEM)
 static const double c_max_chunk_duration = 1.25; // max length of an in-memory chunk in seconds
+#elif defined(__PSP__)
+static const double c_max_chunk_duration = 0.4; // max length of an in-memory chunk in seconds
 #else
 static const double c_max_chunk_duration = 5.0;  // max length of an in-memory chunk in seconds
 #endif
@@ -367,7 +376,7 @@ void InitMixerX()
                       ob.samples);
         }
 
-#if defined(__3DS__)
+#if defined(__3DS__) || defined(__PSP__)
         // Set fastest emulators to be default
         Mix_OPNMIDI_setEmulator(OPNMIDI_OPN2_EMU_GENS);
         Mix_OPNMIDI_setChannelAllocMode(MIX_CHIP_CHANALLOC_SameInst);
