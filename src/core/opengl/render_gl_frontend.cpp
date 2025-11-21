bf89053c4ceb57bfb50ee1abc6caf2b9d4e92cde
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2025-11-21 18:26:16 -0500

    RenderGL: add support for half-pixel mode

--- src/core/opengl/render_gl_frontend.cpp
+++ src/core/opengl/render_gl_frontend.cpp
@@ -572,11 +572,12 @@ void RenderGL::applyViewport()
 
     if(m_use_shaders)
     {
+        // small added epsilon in half-pixel mode makes sure that coordinate rounding occurs correctly
         m_transform_matrix = {
             2.0f / (float)viewport.w, 0.0f, 0.0f, 0.0f,
             0.0f, y_sign * 2.0f / (float)viewport.h, 0.0f, 0.0f,
             0.0f, 0.0f, 1.0f / (float)(1 << 15), 0.0f,
-            -(float)(viewport.w + off.x + off.x) / (viewport.w), -y_sign * (float)(viewport.h + off.y + off.y) / (viewport.h), 0.0f, 1.0f,
+            -(float)(viewport.w + off.x + off.x + 0.1f * m_halfPixelMode) / (viewport.w), -y_sign * (float)(viewport.h + off.y + off.y + 0.1f * m_halfPixelMode) / (viewport.h), 0.0f, 1.0f,
         };
 
         m_shader_read_viewport = {
@@ -641,10 +642,12 @@ void RenderGL::updateViewport()
 
     resetViewport();
 
-    if(ScaleWidth != XRender::TargetW || ScaleHeight != XRender::TargetH || m_current_scale_mode != g_config.scale_mode)
+    bool is_cur_halfpixel = (m_render_scale_factor == 0.5f);
+
+    if(ScaleWidth != XRender::TargetW || ScaleHeight != XRender::TargetH || m_current_scale_mode != g_config.scale_mode || is_cur_halfpixel != m_halfPixelMode)
     {
         // update render targets
-        if(ScaleWidth != XRender::TargetW || ScaleHeight != XRender::TargetH)
+        if(ScaleWidth != XRender::TargetW || ScaleHeight != XRender::TargetH || is_cur_halfpixel != m_halfPixelMode)
         {
 #ifdef PGE_ENABLE_VIDEO_REC
             // invalidates GIF recorder handle
