372bc4b765f3f5c46a30aa90143c5ee28d91b1ea
Wohlstand <admin@wohlnet.ru>
2026-08-04 22:17:44 +0300

    Fully working saving of settings at NSUserDefaults
    All the stuff gets packed into ZIP archive and stored as a field at NSUserDefaults.

--- lib/AppPath/private/app_path_tvos.mm
+++ lib/AppPath/private/app_path_tvos.mm
@@ -44,6 +44,166 @@ static std::string s_gifRecordPath;
 //! The name of application bundle to be re-used as the user directory name
 static std::string s_bundleName;
 
+static bool zipSettingsPack()
+{
+    std::string ar_file = s_userDirectory + "/tmp.zip";
+    mz_zip_archive ar;
+    std::string settings_root = AppPathP::settingsRoot();
+    DirMan setup_dir(settings_root);
+
+    if(!setup_dir.exists())
+        return false; // Do nothing, directory does not exists
+
+    memset(&ar, 0, sizeof(mz_zip_archive));
+
+    Files::deleteFile(ar_file.c_str());
+
+    if(mz_zip_writer_init_file_v2(&ar, ar_file.c_str(), 0, MZ_ZIP_FLAG_WRITE_ALLOW_READING))
+    {
+        std::string cur_path;
+        std::vector<std::string> cur_files;
+        bool failed = false;
+
+        setup_dir.beginWalking();
+
+        while(!failed && setup_dir.fetchListFromWalker(cur_path, cur_files))
+        {
+            for(std::string &file : cur_files)
+            {
+                std::string cur_file = cur_path + "/" + file;
+                std::string cur_relative_file = cur_file.substr(settings_root.size());
+
+                D_pLogDebug("Adding to archive: %s", cur_relative_file.c_str());
+
+                if(!mz_zip_writer_add_file(&ar, cur_relative_file.c_str(), cur_file.c_str(), NULL, 0, MZ_BEST_COMPRESSION|MZ_ZIP_FLAG_WRITE_HEADER_SET_SIZE))
+                {
+                    pLogWarning("tvOS: Failed to store %s into archive %s: %s", cur_relative_file.c_str(), ar_file.c_str(), mz_zip_get_error_string(ar.m_last_error));
+                    failed = true;
+                    break;
+                }
+            }
+        }
+
+        if(!failed)
+            mz_zip_writer_finalize_archive(&ar);
+
+        mz_zip_writer_end(&ar);
+        return !failed;
+    }
+    else
+    {
+        pLogWarning("tvOS: Failed to open an archive %s: %s", ar_file.c_str(), mz_zip_get_error_string(ar.m_last_error));
+    }
+
+    return false;
+}
+
+static void zipSettingsExtract()
+{
+    std::string ar_file = s_userDirectory + "/tmp-out.zip";
+    mz_zip_archive ar;
+    std::string settings_root = AppPathP::settingsRoot();
+    DirMan setup_dir(settings_root);
+
+    if(!Files::fileExists(ar_file))
+        return; // Nothing to do!
+
+    memset(&ar, 0, sizeof(mz_zip_archive));
+
+    if(mz_zip_reader_init_file_v2(&ar, ar_file.c_str(), 0, 0, 0))
+    {
+        mz_uint files = mz_zip_reader_get_num_files(&ar);
+        char file[PATH_MAX];
+        char out_path[PATH_MAX];
+        std::string rel_dir;
+
+        setup_dir.mkpath();
+
+        for(mz_uint i = 0; i < files; ++i)
+        {
+            if(!mz_zip_reader_get_filename(&ar, i, file, PATH_MAX))
+            {
+                pLogWarning("tvOS: Can't get filename for file %d in archive %s: %s", i, ar_file.c_str(),  mz_zip_get_error_string(ar.m_last_error));
+                continue;
+            }
+
+            memset(out_path, 0, sizeof(out_path));
+            strncpy(out_path, settings_root.c_str(), PATH_MAX);
+            strncat(out_path, file, PATH_MAX);
+
+            if(mz_zip_reader_is_file_a_directory(&ar, i))
+            {
+                setup_dir.mkpath(file);
+                continue;
+            }
+
+            rel_dir = Files::dirname(file);
+            setup_dir.mkpath(rel_dir);
+
+            if(!mz_zip_reader_extract_to_file(&ar, i, out_path, 0))
+            {
+                pLogWarning("tvOS: Can't extract file %s from archive %s: %s", file, ar_file.c_str(),  mz_zip_get_error_string(ar.m_last_error));
+                continue;
+            }
+        }
+
+        mz_zip_reader_end(&ar);
+    }
+    else
+    {
+        pLogWarning("tvOS: Can't open ZIP archive %s: %s", ar_file.c_str(),  mz_zip_get_error_string(ar.m_last_error));
+    }
+}
+
+static void saveZipSettings()
+{
+    NSAutoreleasePool *pool;
+    NSData *data;
+    NSUserDefaults *defaults;
+    Files::Data arDara;
+    std::string ar_file = s_userDirectory + "/tmp.zip";
+
+    arDara = Files::load_file(ar_file);
+
+    if(arDara.empty())
+    {
+        pLogWarning("tvOS: Failed to dump the archive %s", ar_file.c_str());
+        return;
+    }
+
+    pool = [[NSAutoreleasePool alloc] init];
+    data = [NSData dataWithBytes:arDara.begin() length:arDara.size()];
+    defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:data forKey:@"settings-zip"];
+    [pool drain];
+}
+
+static bool loadZipSettings()
+{
+    NSAutoreleasePool *pool;
+    NSData *data;
+    NSUserDefaults *defaults;
+    std::string ar_file = s_userDirectory + "/tmp-out.zip";
+    bool ret = false;
+
+    pool = [[NSAutoreleasePool alloc] init];
+    defaults = [NSUserDefaults standardUserDefaults];
+    data = [defaults objectForKey:@"settings-zip"];
+
+    if(data)
+    {
+        FILE *out = fopen(ar_file.c_str(), "wb");
+        if(out)
+        {
+            fwrite([data bytes], 1, [data length], out);
+            fclose(out);
+            ret = true;
+        }
+    }
+
+    [pool drain];
+    return ret;
+}
 
 void AppPathP::initDefaultPaths(const std::string &)
 {
@@ -97,6 +257,10 @@ void AppPathP::initDefaultPaths(const std::string &)
     // Screenshots directory (!!!WILL BE STORED AT CACHES!!!)
     s_screenshotsPath = s_userDirectory + "/screenshots/";
     s_gifRecordPath = s_userDirectory + "/gif-recordings/";
+
+    // Attempt to restore settings directory from a ZIP archive
+    if(!DirMan::exists(AppPathP::settingsRoot()) && loadZipSettings())
+        zipSettingsExtract();
 }
 
 std::string AppPathP::appDirectory()
@@ -157,53 +321,8 @@ bool AppPathP::portableAvailable()
 
 void AppPathP::syncFS()
 {
-    std::string ar_file = s_userDirectory + "/tmp.zip";
-    mz_zip_archive ar;
-    std::string settings_root = settingsRoot();
-    DirMan setup_dir(settings_root);
-
-    if(!setup_dir.exists())
-        return; // Do nothing, directory does not exists
-
-    memset(&ar, 0, sizeof(mz_zip_archive));
-
-    Files::deleteFile(ar_file.c_str());
-
-    if(mz_zip_writer_init_file_v2(&ar, ar_file.c_str(), 0, MZ_ZIP_FLAG_WRITE_ALLOW_READING))
-    {
-        std::string cur_path;
-        std::vector<std::string> cur_files;
-        bool failed = false;
-
-        setup_dir.beginWalking();
-
-        while(!failed && setup_dir.fetchListFromWalker(cur_path, cur_files))
-        {
-            for(std::string &file : cur_files)
-            {
-                std::string cur_file = cur_path + "/" + file;
-                std::string cur_relative_file = cur_file.substr(settings_root.size());
-
-                D_pLogDebug("Adding to archive: %s", cur_relative_file.c_str());
-
-                if(!mz_zip_writer_add_file(&ar, cur_relative_file.c_str(), cur_file.c_str(), NULL, 0, MZ_BEST_COMPRESSION|MZ_ZIP_FLAG_WRITE_HEADER_SET_SIZE))
-                {
-                    pLogWarning("tvOS: Failed to store %s into archive %s: %s", cur_relative_file.c_str(), ar_file.c_str(), mz_zip_get_error_string(ar.m_last_error));
-                    failed = true;
-                    break;
-                }
-            }
-        }
-
-        if(!failed)
-            mz_zip_writer_finalize_archive(&ar);
-
-        mz_zip_writer_end(&ar);
-    }
-    else
-    {
-        pLogWarning("tvOS: Failed to open an archive %s: %s", ar_file.c_str(), mz_zip_get_error_string(ar.m_last_error));
-    }
+    if(zipSettingsPack())
+        saveZipSettings();
 }
 
 const std::vector<std::string>& AppPathManager::worldRootDirs() // Read-Only, appears at writable directory
